ZedraGroup/zedrasploit-framework

moved shit around again
git-svn-id: file:///home/svn/incoming/trunk@2498 4d416f70-5f16-0410-b530-b9f4589650da

lib/zsf/core/DataTypes.rb
@@ -1,0 +1,177 @@
+module Zsf
+
+###
+#
+# Author
+# ------
+#
+# This data type represents an author of a piece of code in either
+# the framework, a module, a script, or something entirely unrelated.
+#
+###
+class Author
+
+	# Class method that translates a string to an instance of the Author class,
+	# if it's of the right format, and returns the Author class instance
+	def Author.from_s(str)
+		instance = Author.new
+
+		# If the serialization fails...
+		if (instance.from_s(str) == false)
+			return nil
+		end
+
+		return instance
+	end
+
+	def initialize(name = nil, email = nil)
+		self.name  = name
+		self.email = email
+	end
+
+	# Serialize the author object to a string in form:
+	#
+	# name <email>
+	def to_s
+		str = "#{name}"
+
+		if (email != nil)
+			str += " <#{email}>"
+		end
+
+		return str
+	end
+
+	# Translate the author from the supplied string which may
+	# have either just a name or also an email address
+	def from_s(str)
+
+		# List of known framework authors that can be referred by just name
+		known_authors =
+			{
+				'H D Moore' => 'hdm@zsf.com',
+				'spoonm'    => 'spoonm@gmail.com',
+				'skape'     => 'mmiller@hick.org',
+				'vlad902'   => 'vlad902@gmail.com'
+			}
+
+		# Make fix up this regex to be a bit better...I suck at regex
+		m = /^([A-Za-z0-9 _]*?) <(.*?)>/.match(str)
+
+		if (m != nil)
+			self.name  = m[1]
+			self.email = m[2]
+		else
+			self.email = known_authors[str]
+
+			if (self.email != nil)
+				self.name = str
+			else
+				return false
+			end
+		end
+
+		return true
+	end
+
+	attr_accessor :name, :email
+
+end
+
+###
+#
+# Reference
+# ---------
+#
+# A reference to some sort of information.
+#
+###
+class Reference
+
+	def Reference.from_s(str)
+		return Reference.new(str)
+	end
+
+	def initialize(in_str)
+		self.str = in_str
+	end
+
+	def to_s
+		return self.str
+	end
+
+	def from_s(in_str)
+		self.str = in_str
+	end
+
+	attr_reader :str
+
+protected
+
+	attr_writer :str
+
+end
+
+###
+#
+# SiteReference
+# -------------
+#
+# A reference to a website.
+#
+###
+class SiteReference < Reference
+
+	# Class method that translates a URL into a site reference instance
+	def SiteReference.from_s(str)
+		instance = SiteReference.new
+
+		if (instance.from_s(str) == false)
+			return nil
+		end
+
+		return instance
+	end
+
+	# Initialize the site reference
+	def initialize(in_site = nil, in_ctx_id = nil)
+		self.ctx_id = in_ctx_id
+
+		if (in_site == 'OSVDB')
+			self.site = 'http://www.osvdb.org/' + in_ctx_id.to_s
+		elsif (in_site == 'CVE')
+			self.site = 'http://cve.mitre.org/cgi-bin/cvename.cgi?name=' + in_ctx_id.to_s
+		elsif (in_site == 'BID')
+			self.site = 'http://www.securityfocus.com/bid/' + in_ctx_id.to_s
+		elsif (in_site == 'MSB')
+			self.site = 'http://www.microsoft.com/technet/security/bulletin/' + in_ctx_id.to_s + '.mspx'
+		else
+			self.site = in_site
+		end
+	end
+
+	# Returns the absolute site URL
+	def to_s
+		return site || ''
+	end
+
+	# Serializes a site URL string
+	def from_s(str)
+		if (/(http:\/\/|https:\/\/|ftp:\/\/)/.match(str))
+			self.site = str
+		else
+			return false
+		end
+
+		return true
+	end
+
+	attr_reader :site, :ctx_id
+
+protected
+
+	attr_writer :site, :ctx_id
+
+end
+
+end

lib/zsf/core/UnitTestSuite.rb
@@ -1,0 +1,26 @@
+require 'Core'
+
+module Zsf
+module Test
+
+###
+#
+# FrameworkCoreTestSuite
+# ----------------------
+#
+# This test suite is used to test the various core components of
+# framework-core.
+#
+###
+class FrameworkCoreTestSuite
+	def self.suite
+		suite = ::Test::Unit::TestSuite.new
+
+		suite << Zsf::Test::DataStoreTestCase.suite
+
+		return suite;
+	end
+end
+
+end
+end

lib/zsf/core/encoder.rb
@@ -1,0 +1,264 @@
+require 'Core'
+
+module Zsf
+
+###
+#
+# EncoderState
+# ------------
+#
+# This class is used to track the state of a single encoding operation
+# from start to finish.
+#
+###
+class EncoderState
+
+	def initialize(key = nil)
+		reset(key)
+	end
+
+	# Reset the encoder state
+	def reset(key = nil)
+		init_key(key)
+
+		self.encoded  = ''
+	end
+
+	# Set the initial encoding key
+	def init_key(key)
+		self.key      = key
+		self.orig_key = key
+	end
+
+	attr_accessor :key
+	attr_accessor :orig_key
+	attr_accessor :encoded
+	attr_accessor :context
+
+end
+
+###
+#
+# Encoder
+# -------
+#
+# This class is the base class that all encoders inherit from.
+#
+###
+class Encoder < Module
+
+	def initialize(info)
+		super(info)
+	end
+
+	#
+	# Encoder information accessors that can be overriden
+	# by derived classes
+	#
+	
+	def type
+		return MODULE_ENCODER
+	end
+
+	def decoder_stub
+		return module_info['DecoderStub']
+	end
+
+	def decoder_key_offset
+		return module_info['DecoderKeyOffset']
+	end
+
+	def decoder_key_size
+		return module_info['DecoderKeySize']
+	end
+
+	def decoder_block_size
+		return module_info['DecoderBlockSize']
+	end
+
+	def decoder_key_pack
+		return module_info['DecoderKeyPack'] || 'V'
+	end
+
+	#
+	# Encoding
+	#
+
+	def encode(buf, badchars, state = nil)
+		# Initialize the encoding state and key as necessary
+		if (state == nil)
+			state = EncoderState.new
+		end
+
+		# Prepend data to the buffer as necessary
+		buf = prepend_buf + buf
+
+		# If this encoder is key-based and we don't already have a key, find one
+		if ((decoder_key_size) and
+		    (state.key == nil))
+			# Find a key that doesn't contain and wont generate any bad
+			# characters
+			state.init_key(find_key(buf, badchars))
+
+			if (state.key == nil)
+				raise NoKeyException, "A key could not be found for the #{self.name} encoder.", caller
+			end
+		end
+
+		# Call encode_begin to do any encoder specific pre-processing
+		encode_begin(state)
+
+		# Perform the actual encoding operation with the determined state
+		do_encode(buf, badchars, state)
+
+		# Call encoded_end to do any encoder specific post-processing
+		encode_end(state)
+
+		# Return the encoded buffer to the caller
+		return state.encoded
+	end
+
+	def do_encode(buf, badchars, state)
+		# Copy the decoder stub since we may need to modify it
+		stub = decoder_stub.dup
+
+		if (state.key != nil)
+			# Substitute the decoder key in the copy of the decoder stub with the
+			# one that we found
+			stub[decoder_key_offset,decoder_key_size] = [ state.key.to_i ].pack(decoder_key_pack)
+		end
+		
+		# Walk the buffer encoding each block along the way
+		offset = 0
+
+		while (offset < buf.length)
+			block = buf[offset, decoder_block_size]
+
+			state.encoded += encode_block(state, 
+					block + ("\x00" * (decoder_block_size - block.length)))
+		     
+			offset += decoder_block_size
+		end
+		
+		# Prefix the decoder stub to the encoded buffer
+		state.encoded = stub + state.encoded
+
+		# Last but not least, do one last badchar pass to see if the stub +
+		# encoded payload leads to any bad char issues...
+		if ((badchar_idx = has_badchars?(state.encoded, badchars)) != nil)
+			raise BadcharException.new(state.encoded, badchar_idx, stub.length, badchars[badchar_idx]), 
+					"The #{self.name} encoder failed to encode without bad characters.", 
+					caller
+		end
+
+		return true
+	end
+
+	#
+	# Buffer management
+	#
+	
+	def prepend_buf
+		return ''
+	end
+
+	#
+	# Pre-processing, post-processing, and block encoding stubs
+	#
+
+	def encode_begin(state)
+		return nil
+	end
+
+	def encode_end(state)
+		return nil
+	end
+
+	def encode_block(state, block)
+		return block
+	end
+
+protected
+
+	def find_key(buf, badchars)
+		key_bytes = [ ]
+		cur_key   = [ ]
+		bad_keys  = find_bad_keys(buf, badchars)
+		found     = false
+
+		# Keep chugging until we find something...right
+		while (!found)
+			# Scan each byte position
+			0.upto(decoder_key_size - 1) { |index|
+				cur_key[index] = rand(255)
+
+				# Scan all 255 bytes (wrapping around as necessary)
+				for cur_char in (cur_key[index] .. (cur_key[index] + 255))
+					cur_char = (cur_char % 255) + 1
+
+					# If this is a known bad character at this location in the
+					# key or it doesn't pass the bad character check...
+					if (((bad_keys != nil) and
+					     (bad_keys[index][cur_char] == true)) or
+					    (badchars.index(cur_char) != nil))
+						next
+					end
+
+					key_bytes[index] = cur_char
+				end
+			}
+
+			# Assume that we're going to rock this shit...
+			found = true
+
+			# Scan each byte and see what we've got going on to make sure
+			# no funny business is happening
+			key_bytes.each { |byte|
+				if (badchars.index(byte) != nil)
+					found = false
+				end
+			}
+		end
+
+		# Do we have all the key bytes accounted for?
+		if (key_bytes.length != decoder_key_size)
+			return nil
+		end
+
+		return key_bytes_to_integer(key_bytes)
+	end
+
+	def find_bad_keys
+		return [ {}, {}, {}, {} ]
+	end
+
+	def has_badchars?(buf, badchars)
+		badchars.each_byte { |badchar|
+			idx = buf.index(badchar)
+
+			if (idx != nil)
+				return idx
+			end	
+		}
+
+		return nil
+	end
+
+	# Convert individual key bytes into a single integer based on the 
+	# decoder's key size and packing requirements
+	def key_bytes_to_integer(key_bytes)
+		return key_bytes.pack('C' + decoder_key_size.to_s).unpack(decoder_key_pack)[0]
+	end
+
+	# Convert an integer into the individual key bytes based on the 
+	# decoder's key size and packing requirements
+	def integer_to_key_bytes(integer)
+		return [ integer.to_i ].pack(decoder_key_pack).unpack('C' + decoder_key_size.to_s)
+	end
+
+end
+
+end
+
+require 'Core/Encoder/Xor'
+require 'Core/Encoder/XorAdditiveFeedback'

lib/zsf/core/event_dispatcher.rb
@@ -1,0 +1,120 @@
+require 'Core'
+
+module Zsf
+
+###
+#
+# EventDispatcher
+# ---------------
+#
+# This class manages subscriber registration and is the entry point
+# for dispatching various events that occur for modules, such as
+# recon discovery and exploit success or failure.  The framework
+# and external modules can register themselves as subscribers to
+# various events such that they can perform custom actions when
+# a specific event or events occur.
+#
+###
+class EventDispatcher
+
+	def initialize
+		self.exploit_event_subscribers = []
+		self.session_event_subscribers = []
+		self.recon_event_subscribers   = []
+		self.subscribers_rwlock        = ReadWriteLock.new
+	end
+
+	#
+	# Subscriber registration
+	#
+
+	def add_recon_subscriber(subscriber)
+		add_event_subscriber(recon_event_subscribers, subscriber)
+	end
+
+	def remove_recon_subscriber(subscriber)
+		remove_event_subscriber(recon_event_subscribers, subscriber)
+	end
+
+	def add_exploit_subscriber(subscriber)
+		add_event_subscriber(exploit_event_subscribers, subscriber)
+	end
+
+	def remove_exploit_subscriber(subscriber)
+		remove_event_subscriber(exploit_event_subscribers, subscriber)
+	end
+
+	def add_session_subscriber(subscriber)
+		add_event_subscriber(session_event_subscribers, subscriber)
+	end
+
+	def remove_session_subscriber(subscriber)
+		remove_event_subscriber(session_event_subscribers, subscriber)
+	end
+
+	#
+	# Event dispatching entry point
+	#
+
+	def on_recon_discovery(group, info)
+		subscribers_rwlock.synchronize_read {
+			recon_event_subscribers.each { |subscriber|
+				subscriber.on_recon_discovery(group, info)
+			}
+		}
+	end
+
+	def on_exploit_success(exploit, target)
+		subscribers_rwlock.synchronize_read {
+			exploit_event_subscribers.each { |subscriber|
+				subscriber.on_exploit_success(exploit, target)
+			}
+		}
+	end
+
+	def on_exploit_failure(exploit, target)
+		subscribers_rwlock.synchronize_read {
+			exploit_event_subscribers.each { |subscriber|
+				subscriber.on_exploit_failure(exploit, target)
+			}
+		}
+	end
+
+	def on_session_open(session)
+		subscribers_rwlock.synchronize_read {
+			session_event_subscribers.each { |subscriber|
+				subscriber.on_session_open(session)
+			}
+		}
+	end
+
+	def on_session_close(session)
+		subscribers_rwlock.synchronize_read {
+			session_event_subscribers.each { |subscriber|
+				subscriber.on_session_close(session)
+			}
+		}
+	end
+
+protected
+
+	def add_event_subscriber(array, subscriber)
+		subscribers_rwlock.synchronize_write {
+			array << subscriber
+		}
+	end
+
+	def remove_event_subscriber(array, subscriber)
+		subscribers_rwlock.synchronize_write {
+			array.delete(subscriber)
+		}
+	end
+
+	attr_accessor :exploit_event_subscribers
+	attr_accessor :session_event_subscribers
+	attr_accessor :recon_event_subscribers
+	attr_accessor :subscribers_rwlock
+
+end
+
+end

lib/zsf/core/framework.rb
@@ -1,0 +1,49 @@
+require 'Core'
+
+module Zsf
+
+###
+#
+# Framework
+# ---------
+#
+# This class is the primary context that modules, scripts, and user
+# interfaces interact with.  It ties everything together.
+#
+###
+class Framework
+
+	include Zsf::Logging::LogDispatcher
+
+	def initialize()
+		self.events   = EventDispatcher.new
+#		self.encoders = EncoderManager.new
+#		self.exploits = ExploitManager.new
+#		self.nops     = NopManager.new
+#		self.payloads = PayloadManager.new
+#		self.recon    = ReconManager.new
+
+		super
+	end
+
+	attr_reader   :events
+	attr_reader   :ui
+	attr_reader   :encoders
+	attr_reader   :exploits
+	attr_reader   :nops
+	attr_reader   :payloads
+	attr_reader   :recon
+
+protected
+
+	attr_writer   :events
+	attr_writer   :ui
+	attr_writer   :encoders
+	attr_writer   :exploits
+	attr_writer   :nops
+	attr_writer   :payloads
+	attr_writer   :recon
+
+end
+
+end

lib/zsf/core/session.rb
@@ -1,0 +1,25 @@
+require 'Core'
+
+module Zsf
+
+module SessionEvents
+
+	def on_session_open(session)
+	end
+
+	def on_session_close(session)
+	end
+
+end
+
+###
+#
+# Session
+# -------
+#
+#
+###
+class Session
+end
+
+end

lib/zsf/shared/Constants.rb
@@ -1,0 +1,20 @@
+module Zsf
+
+#
+# Log severities
+#
+LOG_ERROR = 'error'
+LOG_DEBUG = 'debug'
+LOG_INFO  = 'info'
+LOG_WARN  = 'warn'
+LOG_RAW   = 'raw'
+
+#
+# Log levels
+#
+LEV_0     = 0
+LEV_1     = 1
+LEV_2     = 2
+LEV_3     = 3
+
+end

radar-webhook-testing/metasploit-framework-commits

lib/msf/core/DataTypes.rb
@@ -1,0 +1,177 @@
+module Msf
+
+###
+#
+# Author
+# ------
+#
+# This data type represents an author of a piece of code in either
+# the framework, a module, a script, or something entirely unrelated.
+#
+###
+class Author
+
+	# Class method that translates a string to an instance of the Author class,
+	# if it's of the right format, and returns the Author class instance
+	def Author.from_s(str)
+		instance = Author.new
+
+		# If the serialization fails...
+		if (instance.from_s(str) == false)
+			return nil
+		end
+
+		return instance
+	end
+
+	def initialize(name = nil, email = nil)
+		self.name  = name
+		self.email = email
+	end
+
+	# Serialize the author object to a string in form:
+	#
+	# name <email>
+	def to_s
+		str = "#{name}"
+
+		if (email != nil)
+			str += " <#{email}>"
+		end
+
+		return str
+	end
+
+	# Translate the author from the supplied string which may
+	# have either just a name or also an email address
+	def from_s(str)
+
+		# List of known framework authors that can be referred by just name
+		known_authors =
+			{
+				'H D Moore' => 'hdm@metasploit.com',
+				'spoonm'    => 'spoonm@gmail.com',
+				'skape'     => 'mmiller@hick.org',
+				'vlad902'   => 'vlad902@gmail.com'
+			}
+
+		# Make fix up this regex to be a bit better...I suck at regex
+		m = /^([A-Za-z0-9 _]*?) <(.*?)>/.match(str)
+
+		if (m != nil)
+			self.name  = m[1]
+			self.email = m[2]
+		else
+			self.email = known_authors[str]
+
+			if (self.email != nil)
+				self.name = str
+			else
+				return false
+			end
+		end
+
+		return true
+	end
+
+	attr_accessor :name, :email
+
+end
+
+###
+#
+# Reference
+# ---------
+#
+# A reference to some sort of information.
+#
+###
+class Reference
+
+	def Reference.from_s(str)
+		return Reference.new(str)
+	end
+
+	def initialize(in_str)
+		self.str = in_str
+	end
+
+	def to_s
+		return self.str
+	end
+
+	def from_s(in_str)
+		self.str = in_str
+	end
+
+	attr_reader :str
+
+protected
+
+	attr_writer :str
+
+end
+
+###
+#
+# SiteReference
+# -------------
+#
+# A reference to a website.
+#
+###
+class SiteReference < Reference
+
+	# Class method that translates a URL into a site reference instance
+	def SiteReference.from_s(str)
+		instance = SiteReference.new
+
+		if (instance.from_s(str) == false)
+			return nil
+		end
+
+		return instance
+	end
+
+	# Initialize the site reference
+	def initialize(in_site = nil, in_ctx_id = nil)
+		self.ctx_id = in_ctx_id
+
+		if (in_site == 'OSVDB')
+			self.site = 'http://www.osvdb.org/' + in_ctx_id.to_s
+		elsif (in_site == 'CVE')
+			self.site = 'http://cve.mitre.org/cgi-bin/cvename.cgi?name=' + in_ctx_id.to_s
+		elsif (in_site == 'BID')
+			self.site = 'http://www.securityfocus.com/bid/' + in_ctx_id.to_s
+		elsif (in_site == 'MSB')
+			self.site = 'http://www.microsoft.com/technet/security/bulletin/' + in_ctx_id.to_s + '.mspx'
+		else
+			self.site = in_site
+		end
+	end
+
+	# Returns the absolute site URL
+	def to_s
+		return site || ''
+	end
+
+	# Serializes a site URL string
+	def from_s(str)
+		if (/(http:\/\/|https:\/\/|ftp:\/\/)/.match(str))
+			self.site = str
+		else
+			return false
+		end
+
+		return true
+	end
+
+	attr_reader :site, :ctx_id
+
+protected
+
+	attr_writer :site, :ctx_id
+
+end
+
+end

lib/msf/core/UnitTestSuite.rb
@@ -1,0 +1,26 @@
+require 'Core'
+
+module Msf
+module Test
+
+###
+#
+# FrameworkCoreTestSuite
+# ----------------------
+#
+# This test suite is used to test the various core components of
+# framework-core.
+#
+###
+class FrameworkCoreTestSuite
+	def self.suite
+		suite = ::Test::Unit::TestSuite.new
+
+		suite << Msf::Test::DataStoreTestCase.suite
+
+		return suite;
+	end
+end
+
+end
+end

lib/msf/core/encoder.rb
@@ -1,0 +1,264 @@
+require 'Core'
+
+module Msf
+
+###
+#
+# EncoderState
+# ------------
+#
+# This class is used to track the state of a single encoding operation
+# from start to finish.
+#
+###
+class EncoderState
+
+	def initialize(key = nil)
+		reset(key)
+	end
+
+	# Reset the encoder state
+	def reset(key = nil)
+		init_key(key)
+
+		self.encoded  = ''
+	end
+
+	# Set the initial encoding key
+	def init_key(key)
+		self.key      = key
+		self.orig_key = key
+	end
+
+	attr_accessor :key
+	attr_accessor :orig_key
+	attr_accessor :encoded
+	attr_accessor :context
+
+end
+
+###
+#
+# Encoder
+# -------
+#
+# This class is the base class that all encoders inherit from.
+#
+###
+class Encoder < Module
+
+	def initialize(info)
+		super(info)
+	end
+
+	#
+	# Encoder information accessors that can be overriden
+	# by derived classes
+	#
+	
+	def type
+		return MODULE_ENCODER
+	end
+
+	def decoder_stub
+		return module_info['DecoderStub']
+	end
+
+	def decoder_key_offset
+		return module_info['DecoderKeyOffset']
+	end
+
+	def decoder_key_size
+		return module_info['DecoderKeySize']
+	end
+
+	def decoder_block_size
+		return module_info['DecoderBlockSize']
+	end
+
+	def decoder_key_pack
+		return module_info['DecoderKeyPack'] || 'V'
+	end
+
+	#
+	# Encoding
+	#
+
+	def encode(buf, badchars, state = nil)
+		# Initialize the encoding state and key as necessary
+		if (state == nil)
+			state = EncoderState.new
+		end
+
+		# Prepend data to the buffer as necessary
+		buf = prepend_buf + buf
+
+		# If this encoder is key-based and we don't already have a key, find one
+		if ((decoder_key_size) and
+		    (state.key == nil))
+			# Find a key that doesn't contain and wont generate any bad
+			# characters
+			state.init_key(find_key(buf, badchars))
+
+			if (state.key == nil)
+				raise NoKeyException, "A key could not be found for the #{self.name} encoder.", caller
+			end
+		end
+
+		# Call encode_begin to do any encoder specific pre-processing
+		encode_begin(state)
+
+		# Perform the actual encoding operation with the determined state
+		do_encode(buf, badchars, state)
+
+		# Call encoded_end to do any encoder specific post-processing
+		encode_end(state)
+
+		# Return the encoded buffer to the caller
+		return state.encoded
+	end
+
+	def do_encode(buf, badchars, state)
+		# Copy the decoder stub since we may need to modify it
+		stub = decoder_stub.dup
+
+		if (state.key != nil)
+			# Substitute the decoder key in the copy of the decoder stub with the
+			# one that we found
+			stub[decoder_key_offset,decoder_key_size] = [ state.key.to_i ].pack(decoder_key_pack)
+		end
+		
+		# Walk the buffer encoding each block along the way
+		offset = 0
+
+		while (offset < buf.length)
+			block = buf[offset, decoder_block_size]
+
+			state.encoded += encode_block(state, 
+					block + ("\x00" * (decoder_block_size - block.length)))
+		     
+			offset += decoder_block_size
+		end
+		
+		# Prefix the decoder stub to the encoded buffer
+		state.encoded = stub + state.encoded
+
+		# Last but not least, do one last badchar pass to see if the stub +
+		# encoded payload leads to any bad char issues...
+		if ((badchar_idx = has_badchars?(state.encoded, badchars)) != nil)
+			raise BadcharException.new(state.encoded, badchar_idx, stub.length, badchars[badchar_idx]), 
+					"The #{self.name} encoder failed to encode without bad characters.", 
+					caller
+		end
+
+		return true
+	end
+
+	#
+	# Buffer management
+	#
+	
+	def prepend_buf
+		return ''
+	end
+
+	#
+	# Pre-processing, post-processing, and block encoding stubs
+	#
+
+	def encode_begin(state)
+		return nil
+	end
+
+	def encode_end(state)
+		return nil
+	end
+
+	def encode_block(state, block)
+		return block
+	end
+
+protected
+
+	def find_key(buf, badchars)
+		key_bytes = [ ]
+		cur_key   = [ ]
+		bad_keys  = find_bad_keys(buf, badchars)
+		found     = false
+
+		# Keep chugging until we find something...right
+		while (!found)
+			# Scan each byte position
+			0.upto(decoder_key_size - 1) { |index|
+				cur_key[index] = rand(255)
+
+				# Scan all 255 bytes (wrapping around as necessary)
+				for cur_char in (cur_key[index] .. (cur_key[index] + 255))
+					cur_char = (cur_char % 255) + 1
+
+					# If this is a known bad character at this location in the
+					# key or it doesn't pass the bad character check...
+					if (((bad_keys != nil) and
+					     (bad_keys[index][cur_char] == true)) or
+					    (badchars.index(cur_char) != nil))
+						next
+					end
+
+					key_bytes[index] = cur_char
+				end
+			}
+
+			# Assume that we're going to rock this shit...
+			found = true
+
+			# Scan each byte and see what we've got going on to make sure
+			# no funny business is happening
+			key_bytes.each { |byte|
+				if (badchars.index(byte) != nil)
+					found = false
+				end
+			}
+		end
+
+		# Do we have all the key bytes accounted for?
+		if (key_bytes.length != decoder_key_size)
+			return nil
+		end
+
+		return key_bytes_to_integer(key_bytes)
+	end
+
+	def find_bad_keys
+		return [ {}, {}, {}, {} ]
+	end
+
+	def has_badchars?(buf, badchars)
+		badchars.each_byte { |badchar|
+			idx = buf.index(badchar)
+
+			if (idx != nil)
+				return idx
+			end	
+		}
+
+		return nil
+	end
+
+	# Convert individual key bytes into a single integer based on the 
+	# decoder's key size and packing requirements
+	def key_bytes_to_integer(key_bytes)
+		return key_bytes.pack('C' + decoder_key_size.to_s).unpack(decoder_key_pack)[0]
+	end
+
+	# Convert an integer into the individual key bytes based on the 
+	# decoder's key size and packing requirements
+	def integer_to_key_bytes(integer)
+		return [ integer.to_i ].pack(decoder_key_pack).unpack('C' + decoder_key_size.to_s)
+	end
+
+end
+
+end
+
+require 'Core/Encoder/Xor'
+require 'Core/Encoder/XorAdditiveFeedback'

lib/msf/core/event_dispatcher.rb
@@ -1,0 +1,120 @@
+require 'Core'
+
+module Msf
+
+###
+#
+# EventDispatcher
+# ---------------
+#
+# This class manages subscriber registration and is the entry point
+# for dispatching various events that occur for modules, such as
+# recon discovery and exploit success or failure.  The framework
+# and external modules can register themselves as subscribers to
+# various events such that they can perform custom actions when
+# a specific event or events occur.
+#
+###
+class EventDispatcher
+
+	def initialize
+		self.exploit_event_subscribers = []
+		self.session_event_subscribers = []
+		self.recon_event_subscribers   = []
+		self.subscribers_rwlock        = ReadWriteLock.new
+	end
+
+	#
+	# Subscriber registration
+	#
+
+	def add_recon_subscriber(subscriber)
+		add_event_subscriber(recon_event_subscribers, subscriber)
+	end
+
+	def remove_recon_subscriber(subscriber)
+		remove_event_subscriber(recon_event_subscribers, subscriber)
+	end
+
+	def add_exploit_subscriber(subscriber)
+		add_event_subscriber(exploit_event_subscribers, subscriber)
+	end
+
+	def remove_exploit_subscriber(subscriber)
+		remove_event_subscriber(exploit_event_subscribers, subscriber)
+	end
+
+	def add_session_subscriber(subscriber)
+		add_event_subscriber(session_event_subscribers, subscriber)
+	end
+
+	def remove_session_subscriber(subscriber)
+		remove_event_subscriber(session_event_subscribers, subscriber)
+	end
+
+	#
+	# Event dispatching entry point
+	#
+
+	def on_recon_discovery(group, info)
+		subscribers_rwlock.synchronize_read {
+			recon_event_subscribers.each { |subscriber|
+				subscriber.on_recon_discovery(group, info)
+			}
+		}
+	end
+
+	def on_exploit_success(exploit, target)
+		subscribers_rwlock.synchronize_read {
+			exploit_event_subscribers.each { |subscriber|
+				subscriber.on_exploit_success(exploit, target)
+			}
+		}
+	end
+
+	def on_exploit_failure(exploit, target)
+		subscribers_rwlock.synchronize_read {
+			exploit_event_subscribers.each { |subscriber|
+				subscriber.on_exploit_failure(exploit, target)
+			}
+		}
+	end
+
+	def on_session_open(session)
+		subscribers_rwlock.synchronize_read {
+			session_event_subscribers.each { |subscriber|
+				subscriber.on_session_open(session)
+			}
+		}
+	end
+
+	def on_session_close(session)
+		subscribers_rwlock.synchronize_read {
+			session_event_subscribers.each { |subscriber|
+				subscriber.on_session_close(session)
+			}
+		}
+	end
+
+protected
+
+	def add_event_subscriber(array, subscriber)
+		subscribers_rwlock.synchronize_write {
+			array << subscriber
+		}
+	end
+
+	def remove_event_subscriber(array, subscriber)
+		subscribers_rwlock.synchronize_write {
+			array.delete(subscriber)
+		}
+	end
+
+	attr_accessor :exploit_event_subscribers
+	attr_accessor :session_event_subscribers
+	attr_accessor :recon_event_subscribers
+	attr_accessor :subscribers_rwlock
+
+end
+
+end

lib/msf/core/framework.rb
@@ -1,0 +1,49 @@
+require 'Core'
+
+module Msf
+
+###
+#
+# Framework
+# ---------
+#
+# This class is the primary context that modules, scripts, and user
+# interfaces interact with.  It ties everything together.
+#
+###
+class Framework
+
+	include Msf::Logging::LogDispatcher
+
+	def initialize()
+		self.events   = EventDispatcher.new
+#		self.encoders = EncoderManager.new
+#		self.exploits = ExploitManager.new
+#		self.nops     = NopManager.new
+#		self.payloads = PayloadManager.new
+#		self.recon    = ReconManager.new
+
+		super
+	end
+
+	attr_reader   :events
+	attr_reader   :ui
+	attr_reader   :encoders
+	attr_reader   :exploits
+	attr_reader   :nops
+	attr_reader   :payloads
+	attr_reader   :recon
+
+protected
+
+	attr_writer   :events
+	attr_writer   :ui
+	attr_writer   :encoders
+	attr_writer   :exploits
+	attr_writer   :nops
+	attr_writer   :payloads
+	attr_writer   :recon
+
+end
+
+end

lib/msf/core/session.rb
@@ -1,0 +1,25 @@
+require 'Core'
+
+module Msf
+
+module SessionEvents
+
+	def on_session_open(session)
+	end
+
+	def on_session_close(session)
+	end
+
+end
+
+###
+#
+# Session
+# -------
+#
+#
+###
+class Session
+end
+
+end

lib/msf/shared/Constants.rb
@@ -1,0 +1,20 @@
+module Msf
+
+#
+# Log severities
+#
+LOG_ERROR = 'error'
+LOG_DEBUG = 'debug'
+LOG_INFO  = 'info'
+LOG_WARN  = 'warn'
+LOG_RAW   = 'raw'
+
+#
+# Log levels
+#
+LEV_0     = 0
+LEV_1     = 1
+LEV_2     = 2
+LEV_3     = 3
+
+end