ZedraGroup/zedrasploit-framework

moved things around a bit, added extension iface
git-svn-id: file:///home/svn/incoming/trunk@2354 4d416f70-5f16-0410-b530-b9f4589650da

lib/rex/post/zeterpreter/client.rb
@@ -24,9 +24,11 @@ class Client
 	include Rex::Post::Zeterpreter::PacketDispatcher
 
 	def initialize(sock)
-		self.sock   = sock
-		self.parser = PacketParser.new
-		self.core	= ClientCore.new(self)
+		self.sock       = sock
+		self.parser     = PacketParser.new
+		self.extensions = {}
+
+		self.register_extension_alias('core', ClientCore.new(self))
 
 		monitor_socket
 	end
@@ -37,10 +39,38 @@ class Client
 		return klass
 	end
 
-	attr_reader	  :core
+	#
+	# Pass-thru extensions
+	#
+	def method_missing(symbol, *args)
+		return self.extensions[symbol.to_s];
+	end
+
+	#
+	# Extension registration
+	#
+	def add_extension(name)
+		Kernel.require("Rex/Post/Zeterpreter/Extensions/#{name}")
+
+		ext = eval("Rex::Post::Zeterpreter::Extensions::" + name + ".new(client)")
+
+		self.extensions[ext.name] = ext
+	end
+
+	def register_extension_alias(name, ext)
+		self.extensions[name] = ext
+	end
+
+	def deregister_extension(name)
+		self.extensions.delete(name)
+	end
+
+	def each_extension(&block)
+		self.extensions.each(block)
+	end
+
 	protected
-	attr_accessor :sock, :parser
-	attr_writer	  :core
+	attr_accessor :sock, :parser, :extensions
 end
 
 end; end; end

lib/rex/post/zeterpreter/client_core.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Zeterpreter/Packet'
+require 'Rex/Post/Zeterpreter/Extension'
 
 module Rex
 module Post
@@ -17,10 +18,10 @@ module Zeterpreter
 #
 #
 ###
-class ClientCore
+class ClientCore < Extension
 
     def initialize(client)
-        self.client = client
+        super(client, "core")
     end
 
     #
@@ -164,9 +165,6 @@ class ClientCore
 
 		return true
 	end
-
-	protected
-	attr_accessor :client
 
 end
 

lib/rex/post/zeterpreter/extension.rb
@@ -1,0 +1,19 @@
+#!/usr/bin/ruby
+
+module Rex
+module Post
+module Zeterpreter
+
+class Extension
+
+	def initialize(client, name)
+		self.client = client
+		self.name   = name
+	end
+
+	attr_accessor :name
+	protected
+	attr_accessor :client
+end
+
+end; end; end

radar-webhook-testing/metasploit-framework-commits

lib/rex/post/meterpreter/client.rb
@@ -24,9 +24,11 @@ class Client
 	include Rex::Post::Meterpreter::PacketDispatcher
 
 	def initialize(sock)
-		self.sock   = sock
-		self.parser = PacketParser.new
-		self.core   = ClientCore.new(self)
+		self.sock       = sock
+		self.parser     = PacketParser.new
+		self.extensions = {}
+
+		self.register_extension_alias('core', ClientCore.new(self))
 
 		monitor_socket
 	end
@@ -37,10 +39,38 @@ class Client
 		return klass
 	end
 
-	attr_reader   :core
+	#
+	# Pass-thru extensions
+	#
+	def method_missing(symbol, *args)
+		return self.extensions[symbol.to_s];
+	end
+
+	#
+	# Extension registration
+	#
+	def add_extension(name)
+		Kernel.require("Rex/Post/Meterpreter/Extensions/#{name}")
+
+		ext = eval("Rex::Post::Meterpreter::Extensions::" + name + ".new(client)")
+
+		self.extensions[ext.name] = ext
+	end
+
+	def register_extension_alias(name, ext)
+		self.extensions[name] = ext
+	end
+
+	def deregister_extension(name)
+		self.extensions.delete(name)
+	end
+
+	def each_extension(&block)
+		self.extensions.each(block)
+	end
+
 	protected
-	attr_accessor :sock, :parser
-	attr_writer   :core
+	attr_accessor :sock, :parser, :extensions
 end
 
 end; end; end

lib/rex/post/meterpreter/client_core.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Meterpreter/Packet'
+require 'Rex/Post/Meterpreter/Extension'
 
 module Rex
 module Post
@@ -17,10 +18,10 @@ module Meterpreter
 #
 #
 ###
-class ClientCore
+class ClientCore < Extension
 
 	def initialize(client)
-		self.client = client
+		super(client, "core")	
 	end
 
 	#
@@ -164,9 +165,6 @@ class ClientCore
 
 		return true
 	end
-
-	protected
-	attr_accessor :client
 
 end
 

lib/rex/post/meterpreter/extension.rb
@@ -1,0 +1,19 @@
+#!/usr/bin/ruby
+
+module Rex
+module Post
+module Meterpreter
+
+class Extension
+
+	def initialize(client, name)
+		self.client = client
+		self.name   = name
+	end
+
+	attr_accessor :name
+	protected
+	attr_accessor :client
+end
+
+end; end; end