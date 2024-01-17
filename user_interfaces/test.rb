ZedraGroup/zedrasploit-framework

made each_module better
git-svn-id: file:///home/svn/incoming/trunk@2506 4d416f70-5f16-0410-b530-b9f4589650da

user_interfaces/test.rb
@@ -13,8 +13,13 @@ framework = Zsf::Framework.new
 framework.modules.add_module_path('/home/mmiller/zsf/rubyhacks/prototype/Modules')
 
 
-framework.encoders.each { |name, mod|
-	puts "got encoder #{name} => #{mod}"
+framework.encoders.each_module { |mod|
+	puts "default: got encoder #{mod}"
+}
+
+framework.encoders.each_module(
+		'arch' => Zsf::ARCH_IA32) { |mod|
+	puts "arch filter: got encoder #{mod}"
 }
 
 encoder = framework.encoders.create('JmpCallAdditive')

radar-webhook-testing/metasploit-framework-commits

user_interfaces/test.rb
@@ -13,8 +13,13 @@ framework = Msf::Framework.new
 framework.modules.add_module_path('/home/mmiller/msf/rubyhacks/prototype/Modules')
 
 
-framework.encoders.each { |name, mod|
-	puts "got encoder #{name} => #{mod}"
+framework.encoders.each_module { |mod|
+	puts "default: got encoder #{mod}"
+}
+
+framework.encoders.each_module(
+		'arch' => Msf::ARCH_IA32) { |mod|
+	puts "arch filter: got encoder #{mod}"
 }
 
 encoder = framework.encoders.create('JmpCallAdditive')