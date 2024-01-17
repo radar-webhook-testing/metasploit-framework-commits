ZedraGroup/zedrasploit-framework

moved registry/process around to be under sys, implemented process.memory and process attaching
git-svn-id: file:///home/svn/incoming/trunk@2377 4d416f70-5f16-0410-b530-b9f4589650da

lib/rex/post/zeterpreter/channels/streams/Tcp.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Zeterpreter/Channels/Stream'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Tlv'
 
 module Rex
 module Post
@@ -24,12 +25,18 @@ class Tcp < Rex::Post::Zeterpreter::Channels::Stream
 	##
 
 	def Tcp.open(client, host, port)
-		return Channel.create(client, 'net_stream_tcp',
+		return Channel.create(client, 'stdapi_net_tcp_client',
 			self, CHANNEL_FLAG_SYNCHRONOUS,
 			[
-				{ 'type' => 
-
-			]
+				{ 
+					'type'  => TLV_TYPE_HOST_NAME, 
+					'value' => host
+				},
+				{
+					'type'  => TLV_TYPE_PORT,
+					'value' => port
+				}
+			])
 	end
 
 	##

lib/rex/post/zeterpreter/client.rb
@@ -4,6 +4,7 @@ require 'socket'
 require 'Rex/Post/Zeterpreter/ClientCore'
 require 'Rex/Post/Zeterpreter/Channel'
 require 'Rex/Post/Zeterpreter/ChannelContainer'
+require 'Rex/Post/Zeterpreter/Dependencies'
 require 'Rex/Post/Zeterpreter/ObjectAliases'
 require 'Rex/Post/Zeterpreter/Packet'
 require 'Rex/Post/Zeterpreter/PacketParser'
@@ -106,9 +107,25 @@ class Client
 		self.ext_aliases.aliases[name] = ext
 	end
 
+	# Registers zero or more aliases that are provided in an array
+	def register_extension_aliases(aliases)
+		aliases.each { |a|
+			register_extension_alias(a['name'], a['ext'])
+		}
+	end
+
 	# Deregisters a previously registered extension alias
 	def deregister_extension_alias(name)
 		self.ext_aliases.aliases.delete(name)
+	end
+
+	# Dumps the extension tree
+	def dump_extension_tree()
+		items = []
+		items.concat(self.ext.dump_alias_tree('client.ext'))
+		items.concat(self.ext_aliases.dump_alias_tree('client'))
+
+		return items.sort
 	end
 
 	attr_reader   :ext

lib/rex/post/zeterpreter/extensions/stdapi/stdapi.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/ruby
 
+require 'Rex/Post/Zeterpreter/ObjectAliases'
 require 'Rex/Post/Zeterpreter/Extension'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Constants'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Tlv'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Dir'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/File'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/FileStat'
-require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Process'
-require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Registry'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Sys/Process'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Sys/Registry'
 
 module Rex
 module Post
@@ -28,11 +30,26 @@ class Stdapi < Extension
 
 		# Alias the following things on the client object so that they
 		# can be directly referenced
-		client.register_extension_alias('dir', self.dir)
-		client.register_extension_alias('file', self.file)
-		client.register_extension_alias('filestat', self.filestat)
-		client.register_extension_alias('process', self.process)
-		client.register_extension_alias('registry', self.registry)
+		client.register_extension_aliases(
+			[
+				{ 
+					'name' => 'fs',
+					'ext'  => ObjectAliases.new(
+						{
+							'dir'      => self.dir,
+							'file'     => self.file,
+							'filestat' => self.filestat
+						})
+				},
+				{
+					'name' => 'sys',
+					'ext'  => ObjectAliases.new(
+						{
+							'process'  => self.process,
+							'registry' => self.registry
+						})
+				}
+			])
 	end
 
 	# Sets the client instance on a duplicated copy of the supplied class
@@ -59,12 +76,12 @@ class Stdapi < Extension
 
 	# Returns a copy of the Process class
 	def process
-		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::Process)
+		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::Sys::Process)
 	end
 
 	# Returns a copy of the Registry class
 	def registry
-		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::Registry)
+		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::Sys::Registry)
 	end
 end
 

lib/rex/post/zeterpreter/extensions/stdapi/sys/registry.rb
@@ -5,14 +5,15 @@ require 'Rex/Post/Zeterpreter/Packet'
 require 'Rex/Post/Zeterpreter/Client'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Constants'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Stdapi'
-require 'Rex/Post/Zeterpreter/Extensions/Stdapi/RegistryKey'
-require 'Rex/Post/Zeterpreter/Extensions/Stdapi/RegistryValue'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Sys/RegistrySubsystem/RegistryKey'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Sys/RegistrySubsystem/RegistryValue'
 
 module Rex
 module Post
 module Zeterpreter
 module Extensions
 module Stdapi
+module Sys
 
 ###
 #
@@ -52,8 +53,8 @@ class Registry
 
 		response = client.send_request(request)
 
-		return RegistryKey.new(client, root_key, base_key, perm,
-				response.get_tlv(TLV_TYPE_HKEY).value)
+		return Rex::Post::Zeterpreter::Extensions::Stdapi::Sys::RegistrySubsystem::RegistryKey.new(
+				client, root_key, base_key, perm, response.get_tlv(TLV_TYPE_HKEY).value)
 	end
 
 	# Deletes the supplied registry key.
@@ -150,7 +151,8 @@ class Registry
 			data = data.unpack("N")[0]
 		end
 
-		return RegistryValue.new(client, hkey, name, type, data)
+		return Rex::Post::Zeterpreter::Extensions::Stdapi::Sys::RegistrySubsystem::RegistryValue.new(
+				client, hkey, name, type, data)
 	end
 
 	# Deletes the registry value supplied in name from the supplied
@@ -179,7 +181,8 @@ class Registry
 		response = client.send_request(request)
 
 		response.each(TLV_TYPE_VALUE_NAME) { |value_name|
-			values << RegistryValue.new(client, hkey, value_name.value)
+			values << Rex::Post::Zeterpreter::Extensions::Stdapi::Sys::RegistrySubsystem::RegistryValue.new(
+					client, hkey, value_name.value)
 		}
 
 		return values
@@ -187,4 +190,4 @@ class Registry
 
 end
 
-end; end; end; end; end
+end; end; end; end; end; end

lib/rex/post/zeterpreter/extensions/stdapi/sys/registry_subsystem/registry_key.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Constants'
-require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Registry'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Sys/Registry'
 
 module Rex
 module Post
 module Zeterpreter
 module Extensions
 module Stdapi
+module Sys
+module RegistrySubsystem
 
 ###
 #
@@ -45,13 +47,13 @@ class RegistryKey
 	# Retrieves all of the registry keys that are direct descendents of
 	# the class' registry key.
 	def enum_key()
-		return self.client.registry.enum_key(self.hkey)
+		return self.client.sys.registry.enum_key(self.hkey)
 	end
 
 	# Retrieves all of the registry values that exist within the opened 
 	# registry key.
 	def enum_value()
-		return self.client.registry.enum_value(self.hkey)
+		return self.client.sys.registry.enum_value(self.hkey)
 	end
 
 
@@ -63,24 +65,24 @@ class RegistryKey
 
 	# Opens a registry key that is relative to this registry key.
 	def open_key(base_key, perm = KEY_READ)
-		return self.client.registry.open_key(self.hkey, base_key, perm)
+		return self.client.sys.registry.open_key(self.hkey, base_key, perm)
 	end
 
 	# Creates a registry key that is relative to this registry key.
 	def create_key(base_key, perm = KEY_READ)
-		return self.client.registry.create_key(self.hkey, base_key, perm)
+		return self.client.sys.registry.create_key(self.hkey, base_key, perm)
 	end
 
 	# Deletes a registry key that is relative to this registry key.
 	def delete_key(base_key, recursive = true)
-		return self.client.registry.delete_key(self.hkey, base_key, recursive)
+		return self.client.sys.registry.delete_key(self.hkey, base_key, recursive)
 	end
 
 	# Closes the open key.  This must be called if the registry
 	# key was opened.
 	def close()
 		if (self.hkey != nil)
-			return self.client.registry.close_key(hkey)			
+			return self.client.sys.registry.close_key(hkey)			
 		end
 
 		return false
@@ -94,13 +96,13 @@ class RegistryKey
 
 	# Sets a value relative to the opened registry key.
 	def set_value(name, type, data)
-		return self.client.registry.set_value(self.hkey, name, type, data)
+		return self.client.sys.registry.set_value(self.hkey, name, type, data)
 	end
 
 	# Queries the attributes of the supplied registry value relative to
 	# the opened registry key.
 	def query_value(name)
-		return self.client.registry.query_value(self.hkey, name)
+		return self.client.sys.registry.query_value(self.hkey, name)
 	end
 
 	##
@@ -122,4 +124,4 @@ protected
 	attr_writer   :hkey, :root_key, :base_key, :perm
 end
 
-end; end; end; end; end
+end; end; end; end; end; end; end

lib/rex/post/zeterpreter/extensions/stdapi/sys/registry_subsystem/registry_value.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Constants'
-require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Registry'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Sys/Registry'
 
 module Rex
 module Post
 module Zeterpreter
 module Extensions
 module Stdapi
+module Sys
+module RegistrySubsystem
 
 ###
 #
@@ -31,7 +33,7 @@ class RegistryValue
 		if (type == nil)
 			type = self.type
 		end
-		if (self.client.registry.set_value(self.hkey, self.name,
+		if (self.client.sys.registry.set_value(self.hkey, self.name,
 				type, data))
 			self.data = data
 			self.type = type
@@ -44,7 +46,7 @@ class RegistryValue
 
 	# Queries the value's data.
 	def query()
-		val =  self.client.registry.query_value(self.hkey, self.name)
+		val =  self.client.sys.registry.query_value(self.hkey, self.name)
 
 		if (val != nil)
 			self.data = val.data
@@ -56,7 +58,7 @@ class RegistryValue
 
 	# Deletes the value.
 	def delete()
-		return self.client.registry.delete_value(self.hkey, self.name)
+		return self.client.sys.registry.delete_value(self.hkey, self.name)
 	end
 
 	attr_reader   :hkey, :name, :type, :data
@@ -65,4 +67,4 @@ protected
 	attr_writer   :hkey, :name, :type, :data
 end
 
-end; end; end; end; end
+end; end; end; end; end; end; end

lib/rex/post/zeterpreter/object_aliases.rb
@@ -6,6 +6,54 @@ module Zeterpreter
 
 ###
 #
+# ObjectAliasesContainer
+# ----------------------
+#
+# Mixin for classes that wish to have object aliases but do not
+# really need to inherit from the ObjectAliases class.
+#
+###
+module ObjectAliasesContainer
+
+	# Initialize the instance's aliases
+	def initialize_aliases(aliases = {})
+		self.aliases = aliases
+	end
+
+	# Pass-thru aliases
+	def method_missing(symbol, *args)
+		return self.aliases[symbol.to_s];
+	end
+
+	# Recursively dumps all of the aliases registered with a class that
+	# is kind_of? ObjectAliases
+	def dump_alias_tree(parent_path, current = nil)
+		items = []
+
+		if (current == nil)
+			current = self
+		end
+
+		# If the current object may have object aliases...
+		if (current.kind_of?(Rex::Post::Zeterpreter::ObjectAliases))
+			current.aliases.each_key { |x|
+				current_path = parent_path + '.' + x
+
+				items << current_path
+
+				items.concat(dump_alias_tree(current_path, 
+					current.aliases[x]))
+			}
+		end
+
+		return items
+	end
+
+	attr_accessor :aliases
+end
+
+###
+#
 # ObjectAliases
 # -------------
 #
@@ -14,17 +62,19 @@ module Zeterpreter
 #
 ###
 class ObjectAliases
-
-	def initialize()
-		self.aliases = {}
+	include Rex::Post::Zeterpreter::ObjectAliasesContainer
+	
+	##
+	#
+	# Constructor
+	#
+	##
+	
+	# An instance
+	def initialize(aliases = {})
+		initialize_aliases(aliases)
 	end
-
-	# Pass-thru aliases
-	def method_missing(symbol, *args)
-		return self.aliases[symbol.to_s];
-	end
-
-	attr_accessor :aliases
 end
+
 
 end; end; end

radar-webhook-testing/metasploit-framework-commits

lib/rex/post/meterpreter/channels/streams/Tcp.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Meterpreter/Channels/Stream'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Tlv'
 
 module Rex
 module Post
@@ -24,12 +25,18 @@ class Tcp < Rex::Post::Meterpreter::Channels::Stream
 	##
 
 	def Tcp.open(client, host, port)
-		return Channel.create(client, 'net_stream_tcp',
+		return Channel.create(client, 'stdapi_net_tcp_client',
 			self, CHANNEL_FLAG_SYNCHRONOUS,
 			[
-				{ 'type' => 
-
-			]
+				{ 
+					'type'  => TLV_TYPE_HOST_NAME, 
+					'value' => host
+				},
+				{
+					'type'  => TLV_TYPE_PORT,
+					'value' => port
+				}
+			])
 	end
 
 	##

lib/rex/post/meterpreter/client.rb
@@ -4,6 +4,7 @@ require 'socket'
 require 'Rex/Post/Meterpreter/ClientCore'
 require 'Rex/Post/Meterpreter/Channel'
 require 'Rex/Post/Meterpreter/ChannelContainer'
+require 'Rex/Post/Meterpreter/Dependencies'
 require 'Rex/Post/Meterpreter/ObjectAliases'
 require 'Rex/Post/Meterpreter/Packet'
 require 'Rex/Post/Meterpreter/PacketParser'
@@ -106,9 +107,25 @@ class Client
 		self.ext_aliases.aliases[name] = ext
 	end
 
+	# Registers zero or more aliases that are provided in an array
+	def register_extension_aliases(aliases)
+		aliases.each { |a|
+			register_extension_alias(a['name'], a['ext'])
+		}
+	end
+
 	# Deregisters a previously registered extension alias
 	def deregister_extension_alias(name)
 		self.ext_aliases.aliases.delete(name)
+	end
+
+	# Dumps the extension tree
+	def dump_extension_tree()
+		items = []
+		items.concat(self.ext.dump_alias_tree('client.ext'))
+		items.concat(self.ext_aliases.dump_alias_tree('client'))
+
+		return items.sort
 	end
 
 	attr_reader   :ext

lib/rex/post/meterpreter/extensions/stdapi/stdapi.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/ruby
 
+require 'Rex/Post/Meterpreter/ObjectAliases'
 require 'Rex/Post/Meterpreter/Extension'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Constants'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Tlv'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Dir'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/File'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/FileStat'
-require 'Rex/Post/Meterpreter/Extensions/Stdapi/Process'
-require 'Rex/Post/Meterpreter/Extensions/Stdapi/Registry'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Sys/Process'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Sys/Registry'
 
 module Rex
 module Post
@@ -28,11 +30,26 @@ class Stdapi < Extension
 
 		# Alias the following things on the client object so that they
 		# can be directly referenced
-		client.register_extension_alias('dir', self.dir)
-		client.register_extension_alias('file', self.file)
-		client.register_extension_alias('filestat', self.filestat)
-		client.register_extension_alias('process', self.process)
-		client.register_extension_alias('registry', self.registry)
+		client.register_extension_aliases(
+			[
+				{ 
+					'name' => 'fs',
+					'ext'  => ObjectAliases.new(
+						{
+							'dir'      => self.dir,
+							'file'     => self.file,
+							'filestat' => self.filestat
+						})
+				},
+				{
+					'name' => 'sys',
+					'ext'  => ObjectAliases.new(
+						{
+							'process'  => self.process,
+							'registry' => self.registry
+						})
+				}
+			])
 	end
 
 	# Sets the client instance on a duplicated copy of the supplied class
@@ -59,12 +76,12 @@ class Stdapi < Extension
 
 	# Returns a copy of the Process class
 	def process
-		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Process)
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Sys::Process)
 	end
 
 	# Returns a copy of the Registry class
 	def registry
-		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Registry)
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Sys::Registry)
 	end
 end
 

lib/rex/post/meterpreter/extensions/stdapi/sys/registry.rb
@@ -5,14 +5,15 @@ require 'Rex/Post/Meterpreter/Packet'
 require 'Rex/Post/Meterpreter/Client'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Constants'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Stdapi'
-require 'Rex/Post/Meterpreter/Extensions/Stdapi/RegistryKey'
-require 'Rex/Post/Meterpreter/Extensions/Stdapi/RegistryValue'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Sys/RegistrySubsystem/RegistryKey'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Sys/RegistrySubsystem/RegistryValue'
 
 module Rex
 module Post
 module Meterpreter
 module Extensions
 module Stdapi
+module Sys
 
 ###
 #
@@ -52,8 +53,8 @@ class Registry
 
 		response = client.send_request(request)
 
-		return RegistryKey.new(client, root_key, base_key, perm,
-				response.get_tlv(TLV_TYPE_HKEY).value)
+		return Rex::Post::Meterpreter::Extensions::Stdapi::Sys::RegistrySubsystem::RegistryKey.new(
+				client, root_key, base_key, perm, response.get_tlv(TLV_TYPE_HKEY).value)
 	end
 
 	# Deletes the supplied registry key.
@@ -150,7 +151,8 @@ class Registry
 			data = data.unpack("N")[0]
 		end
 
-		return RegistryValue.new(client, hkey, name, type, data)
+		return Rex::Post::Meterpreter::Extensions::Stdapi::Sys::RegistrySubsystem::RegistryValue.new(
+				client, hkey, name, type, data)
 	end
 
 	# Deletes the registry value supplied in name from the supplied
@@ -179,7 +181,8 @@ class Registry
 		response = client.send_request(request)
 
 		response.each(TLV_TYPE_VALUE_NAME) { |value_name|
-			values << RegistryValue.new(client, hkey, value_name.value)
+			values << Rex::Post::Meterpreter::Extensions::Stdapi::Sys::RegistrySubsystem::RegistryValue.new(
+					client, hkey, value_name.value)
 		}
 
 		return values
@@ -187,4 +190,4 @@ class Registry
 
 end
 
-end; end; end; end; end
+end; end; end; end; end; end

lib/rex/post/meterpreter/extensions/stdapi/sys/registry_subsystem/registry_key.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Constants'
-require 'Rex/Post/Meterpreter/Extensions/Stdapi/Registry'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Sys/Registry'
 
 module Rex
 module Post
 module Meterpreter
 module Extensions
 module Stdapi
+module Sys
+module RegistrySubsystem
 
 ###
 #
@@ -45,13 +47,13 @@ class RegistryKey
 	# Retrieves all of the registry keys that are direct descendents of
 	# the class' registry key.
 	def enum_key()
-		return self.client.registry.enum_key(self.hkey)
+		return self.client.sys.registry.enum_key(self.hkey)
 	end
 
 	# Retrieves all of the registry values that exist within the opened 
 	# registry key.
 	def enum_value()
-		return self.client.registry.enum_value(self.hkey)
+		return self.client.sys.registry.enum_value(self.hkey)
 	end
 
 
@@ -63,24 +65,24 @@ class RegistryKey
 
 	# Opens a registry key that is relative to this registry key.
 	def open_key(base_key, perm = KEY_READ)
-		return self.client.registry.open_key(self.hkey, base_key, perm)
+		return self.client.sys.registry.open_key(self.hkey, base_key, perm)
 	end
 
 	# Creates a registry key that is relative to this registry key.
 	def create_key(base_key, perm = KEY_READ)
-		return self.client.registry.create_key(self.hkey, base_key, perm)
+		return self.client.sys.registry.create_key(self.hkey, base_key, perm)
 	end
 
 	# Deletes a registry key that is relative to this registry key.
 	def delete_key(base_key, recursive = true)
-		return self.client.registry.delete_key(self.hkey, base_key, recursive)
+		return self.client.sys.registry.delete_key(self.hkey, base_key, recursive)
 	end
 
 	# Closes the open key.  This must be called if the registry
 	# key was opened.
 	def close()
 		if (self.hkey != nil)
-			return self.client.registry.close_key(hkey)			
+			return self.client.sys.registry.close_key(hkey)			
 		end
 
 		return false
@@ -94,13 +96,13 @@ class RegistryKey
 
 	# Sets a value relative to the opened registry key.
 	def set_value(name, type, data)
-		return self.client.registry.set_value(self.hkey, name, type, data)
+		return self.client.sys.registry.set_value(self.hkey, name, type, data)
 	end
 
 	# Queries the attributes of the supplied registry value relative to
 	# the opened registry key.
 	def query_value(name)
-		return self.client.registry.query_value(self.hkey, name)
+		return self.client.sys.registry.query_value(self.hkey, name)
 	end
 
 	##
@@ -122,4 +124,4 @@ protected
 	attr_writer   :hkey, :root_key, :base_key, :perm
 end
 
-end; end; end; end; end
+end; end; end; end; end; end; end

lib/rex/post/meterpreter/extensions/stdapi/sys/registry_subsystem/registry_value.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Constants'
-require 'Rex/Post/Meterpreter/Extensions/Stdapi/Registry'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Sys/Registry'
 
 module Rex
 module Post
 module Meterpreter
 module Extensions
 module Stdapi
+module Sys
+module RegistrySubsystem
 
 ###
 #
@@ -31,7 +33,7 @@ class RegistryValue
 		if (type == nil)
 			type = self.type
 		end
-		if (self.client.registry.set_value(self.hkey, self.name,
+		if (self.client.sys.registry.set_value(self.hkey, self.name,
 				type, data))
 			self.data = data
 			self.type = type
@@ -44,7 +46,7 @@ class RegistryValue
 
 	# Queries the value's data.
 	def query()
-		val =  self.client.registry.query_value(self.hkey, self.name)
+		val =  self.client.sys.registry.query_value(self.hkey, self.name)
 
 		if (val != nil)
 			self.data = val.data
@@ -56,7 +58,7 @@ class RegistryValue
 
 	# Deletes the value.
 	def delete()
-		return self.client.registry.delete_value(self.hkey, self.name)
+		return self.client.sys.registry.delete_value(self.hkey, self.name)
 	end
 
 	attr_reader   :hkey, :name, :type, :data
@@ -65,4 +67,4 @@ protected
 	attr_writer   :hkey, :name, :type, :data
 end
 
-end; end; end; end; end
+end; end; end; end; end; end; end

lib/rex/post/meterpreter/object_aliases.rb
@@ -6,6 +6,54 @@ module Meterpreter
 
 ###
 #
+# ObjectAliasesContainer
+# ----------------------
+#
+# Mixin for classes that wish to have object aliases but do not
+# really need to inherit from the ObjectAliases class.
+#
+###
+module ObjectAliasesContainer
+
+	# Initialize the instance's aliases
+	def initialize_aliases(aliases = {})
+		self.aliases = aliases
+	end
+
+	# Pass-thru aliases
+	def method_missing(symbol, *args)
+		return self.aliases[symbol.to_s];
+	end
+
+	# Recursively dumps all of the aliases registered with a class that
+	# is kind_of? ObjectAliases
+	def dump_alias_tree(parent_path, current = nil)
+		items = []
+
+		if (current == nil)
+			current = self
+		end
+
+		# If the current object may have object aliases...
+		if (current.kind_of?(Rex::Post::Meterpreter::ObjectAliases))
+			current.aliases.each_key { |x|
+				current_path = parent_path + '.' + x
+
+				items << current_path
+
+				items.concat(dump_alias_tree(current_path, 
+					current.aliases[x]))
+			}
+		end
+
+		return items
+	end
+
+	attr_accessor :aliases
+end
+
+###
+#
 # ObjectAliases
 # -------------
 #
@@ -14,17 +62,19 @@ module Meterpreter
 #
 ###
 class ObjectAliases
-
-	def initialize()
-		self.aliases = {}
+	include Rex::Post::Meterpreter::ObjectAliasesContainer
+	
+	##
+	#
+	# Constructor
+	#
+	##
+	
+	# An instance
+	def initialize(aliases = {})
+		initialize_aliases(aliases)
 	end
-
-	# Pass-thru aliases
-	def method_missing(symbol, *args)
-		return self.aliases[symbol.to_s];
-	end
-
-	attr_accessor :aliases
 end
+
 
 end; end; end