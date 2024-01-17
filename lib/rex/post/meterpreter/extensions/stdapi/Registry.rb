ZedraGroup/zedrasploit-framework

registry fun stuff
git-svn-id: file:///home/svn/incoming/trunk@2356 4d416f70-5f16-0410-b530-b9f4589650da

lib/rex/post/zeterpreter/extensions/stdapi/Registry.rb
@@ -1,0 +1,41 @@
+#!/usr/bin/ruby
+
+require 'Rex/Post/Process'
+require 'Rex/Post/Zeterpreter/Packet'
+require 'Rex/Post/Zeterpreter/Client'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Constants'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Stdapi'
+
+module Rex
+module Post
+module Zeterpreter
+module Extensions
+module Stdapi
+
+TLV_TYPE_HKEY       = TLV_META_TYPE_UINT   | 1000
+TLV_TYPE_ROOT_KEY   = TLV_TYPE_HKEY
+TLV_TYPE_BASE_KEY   = TLV_META_TYPE_STRING | 1001
+TLV_TYPE_PERMISSION = TLV_META_TYPE_UINT   | 1002
+
+
+class Registry
+
+	class <<self
+		attr_accessor :client
+	end
+
+	def Registry.open_key(root_key, base_key, perm = KEY_READ)
+		request = Packet.create_request('stdapi_registry_open_key')
+
+		request.add_tlv(TLV_TYPE_ROOT_KEY, root_key)
+		request.add_tlv(TLV_TYPE_BASE_KEY, base_key)
+		request.add_tlv(TLV_TYPE_PERMISSION, perm)
+
+		response = self.client.send_request(request)
+
+		return response.get_tlv(TLV_TYPE_HKEY).value
+	end
+
+end
+
+end; end; end; end; end

lib/rex/post/zeterpreter/extensions/stdapi/stdapi.rb
@@ -2,15 +2,13 @@
 
 require 'Rex/Post/Zeterpreter/Extension'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Process'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Registry'
 
 module Rex
 module Post
 module Zeterpreter
 module Extensions
 module Stdapi
-
-# Constants
-TLV_TYPE_PID = TLV_META_TYPE_UINT | 0
 
 ###
 #
@@ -33,6 +31,10 @@ class Stdapi < Extension
 
 	def process
 		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::Process)
+	end
+	
+	def registry
+		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::Registry)
 	end
 end
 

radar-webhook-testing/metasploit-framework-commits

lib/rex/post/meterpreter/extensions/stdapi/Registry.rb
@@ -1,0 +1,41 @@
+#!/usr/bin/ruby
+
+require 'Rex/Post/Process'
+require 'Rex/Post/Meterpreter/Packet'
+require 'Rex/Post/Meterpreter/Client'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Constants'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Stdapi'
+
+module Rex
+module Post
+module Meterpreter
+module Extensions
+module Stdapi
+
+TLV_TYPE_HKEY       = TLV_META_TYPE_UINT   | 1000
+TLV_TYPE_ROOT_KEY   = TLV_TYPE_HKEY
+TLV_TYPE_BASE_KEY   = TLV_META_TYPE_STRING | 1001
+TLV_TYPE_PERMISSION = TLV_META_TYPE_UINT   | 1002
+
+
+class Registry
+
+	class <<self
+		attr_accessor :client
+	end
+
+	def Registry.open_key(root_key, base_key, perm = KEY_READ)
+		request = Packet.create_request('stdapi_registry_open_key')
+
+		request.add_tlv(TLV_TYPE_ROOT_KEY, root_key)
+		request.add_tlv(TLV_TYPE_BASE_KEY, base_key)
+		request.add_tlv(TLV_TYPE_PERMISSION, perm)
+
+		response = self.client.send_request(request)
+
+		return response.get_tlv(TLV_TYPE_HKEY).value
+	end
+
+end
+
+end; end; end; end; end

lib/rex/post/meterpreter/extensions/stdapi/stdapi.rb
@@ -2,15 +2,13 @@
 
 require 'Rex/Post/Meterpreter/Extension'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Process'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Registry'
 
 module Rex
 module Post
 module Meterpreter
 module Extensions
 module Stdapi
-
-# Constants
-TLV_TYPE_PID = TLV_META_TYPE_UINT | 0
 
 ###
 #
@@ -33,6 +31,10 @@ class Stdapi < Extension
 
 	def process
 		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Process)
+	end
+	
+	def registry
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Registry)
 	end
 end
 