ZedraGroup/zedrasploit-framework

worked on the socket api some more, tcp client connections working
git-svn-id: file:///home/svn/incoming/trunk@2437 4d416f70-5f16-0410-b530-b9f4589650da

lib/rex/post/zeterpreter/channels/stream.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Rex/Socket/StreamAbstraction'
+require 'Rex/Post/Zeterpreter/Channel'
 
 module Rex
 module Post
@@ -15,7 +16,7 @@ module Zeterpreter
 # that sequential data is flowing in either one or both directions.
 #
 ###
-class Stream
+class Stream < Rex::Post::Zeterpreter::Channel
 
 	include Rex::Socket::StreamAbstraction
 
@@ -40,7 +41,7 @@ class Stream
 
 	##
 	#
-	# I/O handlers
+	# Remote I/O handlers
 	#
 	##
 

lib/rex/post/zeterpreter/extensions/stdapi/net/socket.rb
@@ -1,0 +1,204 @@
+#!/usr/bin/ruby
+
+require 'thread'
+require 'Rex/Socket/Stream'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Tlv'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Net/SocketSubsystem/TcpClientChannel'
+
+module Rex
+module Post
+module Zeterpreter
+module Extensions
+module Stdapi
+module Net
+
+###
+#
+# Socket
+# ------
+#
+# This class provides an interface to interacting with sockets
+# on the remote machine.  It allows callers to open TCP, UDP,
+# and other arbitrary socket-based connections as channels that
+# can then be interacted with through the established 
+# zeterpreter connection.
+#
+###
+class Socket
+
+	##
+	#
+	# Constructor
+	#
+	##
+
+	# Initialize the socket subsystem and start monitoring sockets
+	# as they come in
+	def initialize(client)
+		self.client                    = client
+		self.monitored_sockets         = []
+		self.monitored_socket_channels = {}
+
+		# Start monitoring shit like the business
+		self.monitor_sockets
+	end
+
+	# Terminate the monitor thread
+	def shutdown
+		monitor_thread.kill
+	end
+
+	##
+	#
+	# Factory
+	#
+	##
+
+	# Creates an arbitrary client socket channel using the information
+	# supplied in the socket parameters instance.  The 'params' argument
+	# is expected to be of type Rex::Socket::Parameters
+	def create(params)
+		channel = nil
+		res     = nil
+
+	#	begin
+			if (params.tcp?)
+				if (params.server?)
+					channel = create_tcp_server(params)
+				else
+					channel = create_tcp_client(params)
+				end
+
+				# Add this channel's right socket to the socket monitor
+				add_monitored_socket(channel.rsock, channel)
+
+				# If we get a valid channel back, create a stream 
+				# representation of the left side of the socket for
+				# the caller to use
+				if (channel != nil)
+					res = Rex::Socket::Stream.new(channel.lsock, nil, nil, nil)
+				end
+			elsif (params.udp?)
+				if (params.server?)
+					res = create_udp_server(params)
+				else
+					res = create_udp_client(params)
+				end
+
+				# TODO: Datagram wrapper
+			end
+	#	rescue
+	#	end
+
+		return res
+	end
+
+	# Create a TCP server channel
+	def create_tcp_server(params)
+	end
+
+	# Creates a TCP client channel
+	def create_tcp_client(params)
+		return SocketSubsystem::TcpClientChannel.open(client, params)
+	end
+
+	# Creates a UDP server channel
+	def create_udp_server(params)
+	end
+
+	# Creates a UDP client channel
+	def create_udp_client(params)
+	end
+
+protected
+
+	##
+	#
+	# Socket monitoring
+	#
+	##
+
+	# Monitors zero or more sockets and handles forwarding traffic
+	# to the remote half of the associated channel
+	def monitor_sockets
+		self.monitor_thread = ::Thread.new {
+
+			while (1)
+		
+				puts "polling..."
+
+				# Watch for data
+				socks = select(monitored_sockets, nil, nil, 1)
+
+				# No data?
+				if (socks == nil || socks[0] == nil)
+					next
+				end
+
+				# Enumerate through each of the indicated sockets
+				socks[0].each { |sock|
+					channel = monitored_socket_channels[sock.object_id]
+					closed  = false
+					data    = nil
+
+					puts "#{sock.object_id}"
+
+					if (channel == nil)
+						remove_monitored_socket(sock)
+
+						next
+					end
+
+					puts "reading..."
+
+					# Read in some data
+					begin
+						data = sock.read(18)
+					rescue
+						puts "closed"
+						closed = true
+					end
+
+					if (data == nil)
+						closed = true
+					end
+
+					puts "sending #{data} to remote"
+
+					# If the socket closed, notify the other side and remove
+					# this socket from the monitored socket list
+					if (closed)
+						channel.close
+
+						remove_monitored_socket(sock)
+					# Otherwise, write the data to the remote side
+					else
+						channel.write(data)
+					end
+				}
+
+			end
+
+		}
+	end
+
+	# Adds a socket to the list of monitored sockets
+	def add_monitored_socket(sock, channel)
+					puts "#{sock.inspect} #{sock.object_id}"
+		monitored_sockets << sock
+		monitored_socket_channels[sock.object_id] = channel
+	end
+
+	# Removes a socket from the list of monitored sockets
+	def remove_monitored_socket(sock)
+		monitored_socket_channels.delete(sock.object_id)
+		monitored_sockets.delete(sock)
+	end
+
+	attr_accessor :monitored_sockets, :monitored_socket_channels
+	attr_accessor :monitor_thread
+	attr_accessor :client
+
+end
+
+end; end; end; end; end; end

lib/rex/post/zeterpreter/extensions/stdapi/net/socket_subsystem/tcp_client_channel.rb
@@ -1,0 +1,74 @@
+#!/usr/bin/ruby
+
+require 'Rex/Post/Zeterpreter/Channels/Stream'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Tlv'
+
+module Rex
+module Post
+module Zeterpreter
+module Extensions
+module Stdapi
+module Net
+module SocketSubsystem
+
+###
+#
+# TcpClientChannel
+# ----------------
+#
+# This class represents a logical TCP client connection
+# that is established from the remote machine and tunnelled
+# through the established zeterpreter connection, similar to an
+# SSH port forward.
+#
+###
+class TcpClientChannel < Rex::Post::Zeterpreter::Stream
+
+	##
+	#
+	# Factory
+	#
+	##
+	
+	# Opens a TCP client channel using the supplied parameters
+	def TcpClientChannel.open(client, params)
+		return Channel.create(client, 'stdapi_net_tcp_client',
+				self, CHANNEL_FLAG_SYNCHRONOUS,
+				[
+					{
+						'type'  => TLV_TYPE_PEER_HOST,
+						'value' => params.peerhost
+					},
+					{
+						'type'  => TLV_TYPE_PEER_PORT,
+						'value' => params.peerport
+					},
+					{
+						'type'  => TLV_TYPE_LOCAL_HOST,
+						'value' => params.localhost
+					},
+					{
+						'type'  => TLV_TYPE_LOCAL_PORT,
+						'value' => params.localport
+					},
+					{
+						'type'  => TLV_TYPE_CONNECT_RETRIES,
+						'value' => params.retries
+					}
+				])
+	end
+
+	##
+	#
+	# Constructor
+	#
+	##
+
+	# Passes the channel initialization information up to the base class
+	def initialize(client, cid, type, flags)
+		super(client, cid, type, flags)
+	end
+
+end
+
+end; end; end; end; end; end; end

lib/rex/post/zeterpreter/extensions/stdapi/stdapi.rb
@@ -8,6 +8,7 @@ require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Fs/Dir'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Fs/File'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Fs/FileStat'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Net/Config'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Net/Socket'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Sys/Process'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Sys/Registry'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/UI'
@@ -55,7 +56,8 @@ class Stdapi < Extension
 					'name' => 'net',
 					'ext'  => ObjectAliases.new(
 						{
-							'config'	=> Rex::Post::Zeterpreter::Extensions::Stdapi::Net::Config.new(client)
+							'config'   => Rex::Post::Zeterpreter::Extensions::Stdapi::Net::Config.new(client),
+							'socket'   => Rex::Post::Zeterpreter::Extensions::Stdapi::Net::Socket.new(client)
 						})
 				},
 				{

radar-webhook-testing/metasploit-framework-commits

lib/rex/post/meterpreter/channels/stream.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Rex/Socket/StreamAbstraction'
+require 'Rex/Post/Meterpreter/Channel'
 
 module Rex
 module Post
@@ -15,7 +16,7 @@ module Meterpreter
 # that sequential data is flowing in either one or both directions.
 #
 ###
-class Stream
+class Stream < Rex::Post::Meterpreter::Channel
 
 	include Rex::Socket::StreamAbstraction
 
@@ -40,7 +41,7 @@ class Stream
 
 	##
 	#
-	# I/O handlers
+	# Remote I/O handlers
 	#
 	##
 

lib/rex/post/meterpreter/extensions/stdapi/net/socket.rb
@@ -1,0 +1,204 @@
+#!/usr/bin/ruby
+
+require 'thread'
+require 'Rex/Socket/Stream'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Tlv'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Net/SocketSubsystem/TcpClientChannel'
+
+module Rex
+module Post
+module Meterpreter
+module Extensions
+module Stdapi
+module Net
+
+###
+#
+# Socket
+# ------
+#
+# This class provides an interface to interacting with sockets
+# on the remote machine.  It allows callers to open TCP, UDP,
+# and other arbitrary socket-based connections as channels that
+# can then be interacted with through the established 
+# meterpreter connection.
+#
+###
+class Socket
+
+	##
+	#
+	# Constructor
+	#
+	##
+
+	# Initialize the socket subsystem and start monitoring sockets
+	# as they come in
+	def initialize(client)
+		self.client                    = client
+		self.monitored_sockets         = []
+		self.monitored_socket_channels = {}
+
+		# Start monitoring shit like the business
+		self.monitor_sockets
+	end
+
+	# Terminate the monitor thread
+	def shutdown
+		monitor_thread.kill
+	end
+
+	##
+	#
+	# Factory
+	#
+	##
+
+	# Creates an arbitrary client socket channel using the information
+	# supplied in the socket parameters instance.  The 'params' argument
+	# is expected to be of type Rex::Socket::Parameters
+	def create(params)
+		channel = nil
+		res     = nil
+
+	#	begin
+			if (params.tcp?)
+				if (params.server?)
+					channel = create_tcp_server(params)
+				else
+					channel = create_tcp_client(params)
+				end
+
+				# Add this channel's right socket to the socket monitor
+				add_monitored_socket(channel.rsock, channel)
+
+				# If we get a valid channel back, create a stream 
+				# representation of the left side of the socket for
+				# the caller to use
+				if (channel != nil)
+					res = Rex::Socket::Stream.new(channel.lsock, nil, nil, nil)
+				end
+			elsif (params.udp?)
+				if (params.server?)
+					res = create_udp_server(params)
+				else
+					res = create_udp_client(params)
+				end
+
+				# TODO: Datagram wrapper
+			end
+	#	rescue
+	#	end
+
+		return res
+	end
+
+	# Create a TCP server channel
+	def create_tcp_server(params)
+	end
+
+	# Creates a TCP client channel
+	def create_tcp_client(params)
+		return SocketSubsystem::TcpClientChannel.open(client, params)
+	end
+
+	# Creates a UDP server channel
+	def create_udp_server(params)
+	end
+
+	# Creates a UDP client channel
+	def create_udp_client(params)
+	end
+
+protected
+
+	##
+	#
+	# Socket monitoring
+	#
+	##
+
+	# Monitors zero or more sockets and handles forwarding traffic
+	# to the remote half of the associated channel
+	def monitor_sockets
+		self.monitor_thread = ::Thread.new {
+
+			while (1)
+		
+				puts "polling..."
+
+				# Watch for data
+				socks = select(monitored_sockets, nil, nil, 1)
+
+				# No data?
+				if (socks == nil || socks[0] == nil)
+					next
+				end
+
+				# Enumerate through each of the indicated sockets
+				socks[0].each { |sock|
+					channel = monitored_socket_channels[sock.object_id]
+					closed  = false
+					data    = nil
+
+					puts "#{sock.object_id}"
+
+					if (channel == nil)
+						remove_monitored_socket(sock)
+
+						next
+					end
+
+					puts "reading..."
+
+					# Read in some data
+					begin
+						data = sock.read(18)
+					rescue
+						puts "closed"
+						closed = true
+					end
+
+					if (data == nil)
+						closed = true
+					end
+
+					puts "sending #{data} to remote"
+
+					# If the socket closed, notify the other side and remove
+					# this socket from the monitored socket list
+					if (closed)
+						channel.close
+
+						remove_monitored_socket(sock)
+					# Otherwise, write the data to the remote side
+					else
+						channel.write(data)
+					end
+				}
+
+			end
+
+		}
+	end
+
+	# Adds a socket to the list of monitored sockets
+	def add_monitored_socket(sock, channel)
+					puts "#{sock.inspect} #{sock.object_id}"
+		monitored_sockets << sock
+		monitored_socket_channels[sock.object_id] = channel
+	end
+
+	# Removes a socket from the list of monitored sockets
+	def remove_monitored_socket(sock)
+		monitored_socket_channels.delete(sock.object_id)
+		monitored_sockets.delete(sock)
+	end
+
+	attr_accessor :monitored_sockets, :monitored_socket_channels
+	attr_accessor :monitor_thread
+	attr_accessor :client
+
+end
+
+end; end; end; end; end; end

lib/rex/post/meterpreter/extensions/stdapi/net/socket_subsystem/tcp_client_channel.rb
@@ -1,0 +1,74 @@
+#!/usr/bin/ruby
+
+require 'Rex/Post/Meterpreter/Channels/Stream'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Tlv'
+
+module Rex
+module Post
+module Meterpreter
+module Extensions
+module Stdapi
+module Net
+module SocketSubsystem
+
+###
+#
+# TcpClientChannel
+# ----------------
+#
+# This class represents a logical TCP client connection
+# that is established from the remote machine and tunnelled
+# through the established meterpreter connection, similar to an
+# SSH port forward.
+#
+###
+class TcpClientChannel < Rex::Post::Meterpreter::Stream
+
+	##
+	#
+	# Factory
+	#
+	##
+	
+	# Opens a TCP client channel using the supplied parameters
+	def TcpClientChannel.open(client, params)
+		return Channel.create(client, 'stdapi_net_tcp_client',
+				self, CHANNEL_FLAG_SYNCHRONOUS,
+				[
+					{
+						'type'  => TLV_TYPE_PEER_HOST,
+						'value' => params.peerhost
+					},
+					{
+						'type'  => TLV_TYPE_PEER_PORT,
+						'value' => params.peerport
+					},
+					{
+						'type'  => TLV_TYPE_LOCAL_HOST,
+						'value' => params.localhost
+					},
+					{
+						'type'  => TLV_TYPE_LOCAL_PORT,
+						'value' => params.localport
+					},
+					{
+						'type'  => TLV_TYPE_CONNECT_RETRIES,
+						'value' => params.retries
+					}
+				])
+	end
+
+	##
+	#
+	# Constructor
+	#
+	##
+
+	# Passes the channel initialization information up to the base class
+	def initialize(client, cid, type, flags)
+		super(client, cid, type, flags)
+	end
+
+end
+
+end; end; end; end; end; end; end

lib/rex/post/meterpreter/extensions/stdapi/stdapi.rb
@@ -8,6 +8,7 @@ require 'Rex/Post/Meterpreter/Extensions/Stdapi/Fs/Dir'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Fs/File'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Fs/FileStat'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Net/Config'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Net/Socket'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Sys/Process'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Sys/Registry'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/UI'
@@ -55,7 +56,8 @@ class Stdapi < Extension
 					'name' => 'net',
 					'ext'  => ObjectAliases.new(
 						{
-							'config'   => Rex::Post::Meterpreter::Extensions::Stdapi::Net::Config.new(client)
+							'config'   => Rex::Post::Meterpreter::Extensions::Stdapi::Net::Config.new(client),
+							'socket'   => Rex::Post::Meterpreter::Extensions::Stdapi::Net::Socket.new(client)
 						})
 				},	
 				{