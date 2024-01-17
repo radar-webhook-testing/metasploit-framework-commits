ZedraGroup/zedrasploit-framework

text user interface crap
git-svn-id: file:///home/svn/incoming/trunk@2508 4d416f70-5f16-0410-b530-b9f4589650da

lib/zsf/ui.rb
@@ -1,0 +1,7 @@
+module Zsf
+module Ui
+end
+end
+
+require 'Zsf/Ui/Driver'
+require 'Zsf/Ui/Console'

lib/zsf/ui/console.rb
@@ -1,0 +1,10 @@
+module Zsf
+module Ui
+module Console
+
+
+end
+end
+end
+
+require 'zsf/Ui/Console/Driver'

lib/zsf/ui/console/command_dispatcher.rb
@@ -1,0 +1,52 @@
+module zsf
+module Ui
+module Console
+
+module CommandDispatcher
+
+	def initialize(in_driver)
+		self.driver = in_driver
+	end
+
+	def print_error(msg)
+		driver.print_error(msg)
+	end
+
+	def print_status(msg)
+		driver.print_status(msg)
+	end
+
+	def print_line(msg)
+		driver.print_line(msg)
+	end
+
+	def print(msg)
+		driver.print(msg)
+	end
+
+	def update_prompt(prompt)
+		driver.update_prompt(prompt)
+	end
+
+	def framework
+		return driver.framework
+	end
+
+	def set_active_module(mod)
+		driver.datastore['_ActiveModule'] = mod
+	end
+
+	def get_active_module
+		return driver.datastore['_ActiveModule']
+	end
+
+protected
+
+	attr_accessor :driver
+
+end 
+
+end end end
+
+require 'zsf/Ui/Console/ModuleCommandDispatcher'
+require 'zsf/Ui/Console/CommandDispatcher/Core'

lib/zsf/ui/console/input_methods.rb
@@ -1,0 +1,68 @@
+require 'zsf/Ui'
+
+module zsf
+module Ui
+module Console
+
+###
+#
+# InputMethod
+# -----------
+#
+# This class is the base class for the three kinds of console input methods:
+# Stdio, File, and Readline.  These classes are nearly idential to the classes
+# found in irb.
+#
+###
+class InputMethod
+	def initialize
+		self.eof = false
+	end
+
+	def gets
+		raise NotImplementedError
+	end
+
+	def eof?
+		return eof
+	end
+
+	attr_accessor :prompt, :eof
+end
+
+class StdioInputMethod < InputMethod
+	def gets
+		print prompt
+		return $stdin.gets
+	end
+
+	def eof?
+		return $stdin.eof?
+	end
+end
+
+begin 
+	require 'readline'
+
+	class ReadlineInputMethod < InputMethod
+		include Readline
+
+		def gets
+			if ((line = readline(prompt, true)))
+				HISTORY.pop if (line.empty?)
+				return line + "\n"
+			else
+				eof = true
+				return line
+			end
+		end
+	end
+rescue LoadError
+end
+
+class FileInputMethod < InputMethod
+end
+
+end
+end
+end

lib/zsf/ui/console/module_command_dispatcher.rb
@@ -1,0 +1,15 @@
+module zsf
+module Ui
+module Console
+
+module ModuleCommandDispatcher
+
+	include zsf::Ui::Console::CommandDispatcher
+
+	def mod
+		return get_active_module
+	end
+
+end
+
+end end end

lib/zsf/ui/console/output_methods.rb
@@ -1,0 +1,28 @@
+module zsf
+module Ui
+module Console
+
+class OutputMethod
+end
+
+class StdioOutputMethod < OutputMethod
+	def print_error(msg)
+		print_line("[*] #{msg}")
+	end
+
+	def print_status(msg)
+		print_line("[-] #{msg}")
+	end
+
+	def print_line(msg)
+		print(msg + "\n")
+	end
+
+	def print(msg)
+		$stdout.print(msg)
+	end
+end
+
+end
+end
+end

lib/zsf/ui/console/shell.rb
@@ -1,0 +1,96 @@
+require 'zsf/Ui'
+require 'zsf/Ui/Console/InputMethods'
+require 'zsf/Ui/Console/OutputMethods'
+
+module zsf
+module Ui
+module Console
+
+###
+#
+# Shell
+# -----
+#
+# The shell class provides a command-prompt style interface in a 
+# generic fashion.
+#
+###
+module Shell
+
+	def initialize(prompt)
+		# Initialize the input and output methods
+		self.input  = StdioInputMethod.new
+		self.output = StdioOutputMethod.new
+
+		begin
+			self.input = ReadlineInputMethod.new
+		rescue
+		end
+
+		# Set the stop flag to false
+		self.stop_flag = false
+
+		# Initialize the prompt
+		self.init_prompt = prompt
+
+		update_prompt
+
+		super()
+	end
+
+	# Run the command processing loop
+	def run
+		stop_flag = false
+
+		while ((line = input.gets))
+			run_single(line)
+
+			break if (input.eof? or self.stop_flag)
+		end
+	end
+
+	# Stop processing user input
+	def stop
+		self.stop_flag = true
+	end
+
+	# Change the input prompt
+	def update_prompt(prompt = '')
+		self.input.prompt = self.init_prompt + ' ' + prompt + '> '
+	end
+
+	#
+	# Output shortcuts
+	#
+	
+	def print_error(msg)
+		output.print_error(msg)
+	end
+
+	def print_status(msg)
+		output.print_status(msg)
+	end
+
+	def print_line(msg)
+		output.print_line(msg)
+	end
+
+	def print(msg)
+		output.print(msg)
+	end
+
+protected
+
+	# Parse a line into an array of arguments
+	def parse_line(line)
+		line.gsub!("(\r|\n)", '')
+
+		args = line.split(' ')
+	end
+
+
+	attr_accessor :input, :output, :stop_flag, :init_prompt
+
+end
+
+end end end

radar-webhook-testing/metasploit-framework-commits

lib/msf/ui.rb
@@ -1,0 +1,7 @@
+module Msf
+module Ui
+end
+end
+
+require 'Msf/Ui/Driver'
+require 'Msf/Ui/Console'

lib/msf/ui/console.rb
@@ -1,0 +1,10 @@
+module Msf
+module Ui
+module Console
+
+
+end
+end
+end
+
+require 'Msf/Ui/Console/Driver'

lib/msf/ui/console/command_dispatcher.rb
@@ -1,0 +1,52 @@
+module Msf
+module Ui
+module Console
+
+module CommandDispatcher
+
+	def initialize(in_driver)
+		self.driver = in_driver
+	end
+
+	def print_error(msg)
+		driver.print_error(msg)
+	end
+
+	def print_status(msg)
+		driver.print_status(msg)
+	end
+
+	def print_line(msg)
+		driver.print_line(msg)
+	end
+
+	def print(msg)
+		driver.print(msg)
+	end
+
+	def update_prompt(prompt)
+		driver.update_prompt(prompt)
+	end
+
+	def framework
+		return driver.framework
+	end
+
+	def set_active_module(mod)
+		driver.datastore['_ActiveModule'] = mod
+	end
+
+	def get_active_module
+		return driver.datastore['_ActiveModule']
+	end
+
+protected
+
+	attr_accessor :driver
+
+end 
+
+end end end
+
+require 'Msf/Ui/Console/ModuleCommandDispatcher'
+require 'Msf/Ui/Console/CommandDispatcher/Core'

lib/msf/ui/console/input_methods.rb
@@ -1,0 +1,68 @@
+require 'Msf/Ui'
+
+module Msf
+module Ui
+module Console
+
+###
+#
+# InputMethod
+# -----------
+#
+# This class is the base class for the three kinds of console input methods:
+# Stdio, File, and Readline.  These classes are nearly idential to the classes
+# found in irb.
+#
+###
+class InputMethod
+	def initialize
+		self.eof = false
+	end
+
+	def gets
+		raise NotImplementedError
+	end
+
+	def eof?
+		return eof
+	end
+
+	attr_accessor :prompt, :eof
+end
+
+class StdioInputMethod < InputMethod
+	def gets
+		print prompt
+		return $stdin.gets
+	end
+
+	def eof?
+		return $stdin.eof?
+	end
+end
+
+begin 
+	require 'readline'
+
+	class ReadlineInputMethod < InputMethod
+		include Readline
+
+		def gets
+			if ((line = readline(prompt, true)))
+				HISTORY.pop if (line.empty?)
+				return line + "\n"
+			else
+				eof = true
+				return line
+			end
+		end
+	end
+rescue LoadError
+end
+
+class FileInputMethod < InputMethod
+end
+
+end
+end
+end

lib/msf/ui/console/module_command_dispatcher.rb
@@ -1,0 +1,15 @@
+module Msf
+module Ui
+module Console
+
+module ModuleCommandDispatcher
+
+	include Msf::Ui::Console::CommandDispatcher
+
+	def mod
+		return get_active_module
+	end
+
+end
+
+end end end

lib/msf/ui/console/output_methods.rb
@@ -1,0 +1,28 @@
+module Msf
+module Ui
+module Console
+
+class OutputMethod
+end
+
+class StdioOutputMethod < OutputMethod
+	def print_error(msg)
+		print_line("[*] #{msg}")
+	end
+
+	def print_status(msg)
+		print_line("[-] #{msg}")
+	end
+
+	def print_line(msg)
+		print(msg + "\n")
+	end
+
+	def print(msg)
+		$stdout.print(msg)
+	end
+end
+
+end
+end
+end

lib/msf/ui/console/shell.rb
@@ -1,0 +1,96 @@
+require 'Msf/Ui'
+require 'Msf/Ui/Console/InputMethods'
+require 'Msf/Ui/Console/OutputMethods'
+
+module Msf
+module Ui
+module Console
+
+###
+#
+# Shell
+# -----
+#
+# The shell class provides a command-prompt style interface in a 
+# generic fashion.
+#
+###
+module Shell
+
+	def initialize(prompt)
+		# Initialize the input and output methods
+		self.input  = StdioInputMethod.new
+		self.output = StdioOutputMethod.new
+
+		begin
+			self.input = ReadlineInputMethod.new
+		rescue
+		end
+
+		# Set the stop flag to false
+		self.stop_flag = false
+
+		# Initialize the prompt
+		self.init_prompt = prompt
+
+		update_prompt
+
+		super()
+	end
+
+	# Run the command processing loop
+	def run
+		stop_flag = false
+
+		while ((line = input.gets))
+			run_single(line)
+
+			break if (input.eof? or self.stop_flag)
+		end
+	end
+
+	# Stop processing user input
+	def stop
+		self.stop_flag = true
+	end
+
+	# Change the input prompt
+	def update_prompt(prompt = '')
+		self.input.prompt = self.init_prompt + ' ' + prompt + '> '
+	end
+
+	#
+	# Output shortcuts
+	#
+	
+	def print_error(msg)
+		output.print_error(msg)
+	end
+
+	def print_status(msg)
+		output.print_status(msg)
+	end
+
+	def print_line(msg)
+		output.print_line(msg)
+	end
+
+	def print(msg)
+		output.print(msg)
+	end
+
+protected
+
+	# Parse a line into an array of arguments
+	def parse_line(line)
+		line.gsub!("(\r|\n)", '')
+
+		args = line.split(' ')
+	end
+
+
+	attr_accessor :input, :output, :stop_flag, :init_prompt
+
+end
+
+end end end