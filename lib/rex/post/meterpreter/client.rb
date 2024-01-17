ZedraGroup/zedrasploit-framework

this is all to my baby mommas out there
git-svn-id: file:///home/svn/incoming/trunk@2357 4d416f70-5f16-0410-b530-b9f4589650da

lib/rex/post/zeterpreter/client.rb
@@ -48,11 +48,15 @@ class Client
 	# Extension registration
 	#
 	def add_extension(name)
-		Kernel.require("Rex/Post/Zeterpreter/Extensions/#{name}/#{name}")
+		if (Kernel.require("Rex/Post/Zeterpreter/Extensions/#{name}/#{name}") == false)
+			return false
+		end
 
 		ext = eval("Rex::Post::Zeterpreter::Extensions::" + name + "::" + name + ".new(self)")
 
 		self.extensions[ext.name] = ext
+
+		return true
 	end
 
 	def register_extension_alias(name, ext)

radar-webhook-testing/metasploit-framework-commits

lib/rex/post/meterpreter/client.rb
@@ -48,11 +48,15 @@ class Client
 	# Extension registration
 	#
 	def add_extension(name)
-		Kernel.require("Rex/Post/Meterpreter/Extensions/#{name}/#{name}")
+		if (Kernel.require("Rex/Post/Meterpreter/Extensions/#{name}/#{name}") == false)
+			return false
+		end
 
 		ext = eval("Rex::Post::Meterpreter::Extensions::" + name + "::" + name + ".new(self)")
 
 		self.extensions[ext.name] = ext
+
+		return true
 	end
 
 	def register_extension_alias(name, ext)