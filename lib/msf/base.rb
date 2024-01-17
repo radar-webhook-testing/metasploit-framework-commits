ZedraGroup/zedrasploit-framework

started working on session wrappers
git-svn-id: file:///home/svn/incoming/trunk@2529 4d416f70-5f16-0410-b530-b9f4589650da

lib/zsf/base.rb
@@ -1,0 +1,25 @@
+###
+#
+# framework-base
+# --------------
+#
+# The base library provides implementations for some of the default
+# sessions, such as Shell, Zeterpreter, DispatchNinja, and VNC.  These
+# sessions are used by modules that come pre-packaged with the default
+# module distribution of Zedrasploit and are depended on by their 
+# respective payloads.
+#
+# Beyond providing the default sessions, framework-base also provides
+# a wrapper interface to framework-core that makes some of the tasks,
+# such as exploitation, into easier to manage functions.
+#
+###
+
+# framework-base depends on framework-core
+require 'Zsf/Core'
+
+# Sessions
+require 'Zsf/Base/Session/CommandShell'
+require 'Zsf/Base/Session/Zeterpreter'
+require 'Zsf/Base/Session/DispatchNinja'
+require 'Zsf/Base/Session/Vnc'

lib/zsf/base.rb.ts.rb
@@ -1,0 +1,27 @@
+#!/usr/bin/ruby
+
+require 'test/unit'
+require 'Zsf/Base'
+require 'Zsf/Base/Session/CommandShell.rb.ut'
+require 'Zsf/Base/Session/Zeterpreter.rb.ut'
+require 'Zsf/Base/Session/DispatchNinja.rb.ut'
+require 'Zsf/Base/Session/Vnc.rb.ut'
+
+module Zsf
+module Base
+
+class TestSuite
+	def self.suite
+		suite = Test::Unit::TestSuite.new
+
+		suite << Zsf::Session::CommandShell::UnitTest.suite
+		suite << Zsf::Session::Zeterpreter::UnitTest.suite
+		suite << Zsf::Session::DispatchNinja::UnitTest.suite
+		suite << Zsf::Session::Vnc::UnitTest.suite
+
+		return suite;
+	end
+end
+
+end
+end

lib/zsf/core/session.rb
@@ -57,3 +57,11 @@ protected
 end
 
 end
+
+# 
+# Require the individual provider interfaces
+#
+require 'Zsf/Core/SessionProvider/SingleCommandExecution'
+require 'Zsf/Core/SessionProvider/MultiCommandExecution'
+require 'Zsf/Core/SessionProvider/SingleCommandShell'
+require 'Zsf/Core/SessionProvider/MultiCommandShell'

lib/rex/post/zeterpreter/client_core.rb
@@ -133,31 +133,18 @@ class ClientCore < Extension
 		Indicates that the library should be loaded from disk, not from
 		memory on the remote machine
 =end
-	def use(opts)
-		modules = []
-
-		if (opts['Module'])
-			modules << opts['Module']
-		elsif (opts['Modules'])
-			modules = opts['Modules']
-		end
-
-		if (modules.length == 0)
+	def use(module, opts)
+		if (module == nil)
 			raise RuntimeError, "No modules were specified", caller
 		end
 
-		# Enumerate all of the modules, loading each one
-		modules.each { |mod|
-
-			if (load_library(
-					'LibraryFilePath' => 'data/zeterpreter/ext_server_' + mod.downcase + '.dll',
-					'UploadLibrary'   => true,
-					'Extension'       => true,
-					'SaveToDisk'      => opts['LoadFromDisk']))
-				client.add_extension(mod)
-			end
-			
-		}
+		if (load_library(
+				'LibraryFilePath' => 'data/zeterpreter/ext_server_' + module.downcase + '.dll',
+				'UploadLibrary'   => true,
+				'Extension'       => true,
+				'SaveToDisk'      => opts['LoadFromDisk']))
+			client.add_extension(mod)
+		end
 
 		return true
 	end

user_interfaces/tc.rb
@@ -2,5 +2,7 @@
 
 require 'test/unit/ui/console/testrunner'
 require 'zsf/Core.rb.ts'
+require 'Zsf/Base.rb.ts'
 
-Test::Unit::UI::Console::TestRunner.run(zsf::TestSuite)
+Test::Unit::UI::Console::TestRunner.run(Zsf::TestSuite
+Test::Unit::UI::Console::TestRunner.run(Zsf::Base::TestSuite)

radar-webhook-testing/metasploit-framework-commits

lib/msf/base.rb
@@ -1,0 +1,25 @@
+###
+#
+# framework-base
+# --------------
+#
+# The base library provides implementations for some of the default
+# sessions, such as Shell, Meterpreter, DispatchNinja, and VNC.  These
+# sessions are used by modules that come pre-packaged with the default
+# module distribution of Metasploit and are depended on by their 
+# respective payloads.
+#
+# Beyond providing the default sessions, framework-base also provides
+# a wrapper interface to framework-core that makes some of the tasks,
+# such as exploitation, into easier to manage functions.
+#
+###
+
+# framework-base depends on framework-core
+require 'Msf/Core'
+
+# Sessions
+require 'Msf/Base/Session/CommandShell'
+require 'Msf/Base/Session/Meterpreter'
+require 'Msf/Base/Session/DispatchNinja'
+require 'Msf/Base/Session/Vnc'

lib/msf/base.rb.ts.rb
@@ -1,0 +1,27 @@
+#!/usr/bin/ruby
+
+require 'test/unit'
+require 'Msf/Base'
+require 'Msf/Base/Session/CommandShell.rb.ut'
+require 'Msf/Base/Session/Meterpreter.rb.ut'
+require 'Msf/Base/Session/DispatchNinja.rb.ut'
+require 'Msf/Base/Session/Vnc.rb.ut'
+
+module Msf
+module Base
+
+class TestSuite
+	def self.suite
+		suite = Test::Unit::TestSuite.new
+
+		suite << Msf::Session::CommandShell::UnitTest.suite
+		suite << Msf::Session::Meterpreter::UnitTest.suite
+		suite << Msf::Session::DispatchNinja::UnitTest.suite
+		suite << Msf::Session::Vnc::UnitTest.suite
+
+		return suite;
+	end
+end
+
+end
+end

lib/msf/core/session.rb
@@ -57,3 +57,11 @@ protected
 end
 
 end
+
+# 
+# Require the individual provider interfaces
+#
+require 'Msf/Core/SessionProvider/SingleCommandExecution'
+require 'Msf/Core/SessionProvider/MultiCommandExecution'
+require 'Msf/Core/SessionProvider/SingleCommandShell'
+require 'Msf/Core/SessionProvider/MultiCommandShell'

lib/rex/post/meterpreter/client_core.rb
@@ -133,31 +133,18 @@ class ClientCore < Extension
 		Indicates that the library should be loaded from disk, not from
 		memory on the remote machine
 =end
-	def use(opts)
-		modules = []
-
-		if (opts['Module'])
-			modules << opts['Module']
-		elsif (opts['Modules'])
-			modules = opts['Modules']
-		end
-
-		if (modules.length == 0)
+	def use(module, opts)
+		if (module == nil)
 			raise RuntimeError, "No modules were specified", caller
 		end
 
-		# Enumerate all of the modules, loading each one
-		modules.each { |mod|
-
-			if (load_library(
-					'LibraryFilePath' => 'data/meterpreter/ext_server_' + mod.downcase + '.dll',
-					'UploadLibrary'   => true,
-					'Extension'       => true,
-					'SaveToDisk'      => opts['LoadFromDisk']))
-				client.add_extension(mod)
-			end
-			
-		}
+		if (load_library(
+				'LibraryFilePath' => 'data/meterpreter/ext_server_' + module.downcase + '.dll',
+				'UploadLibrary'   => true,
+				'Extension'       => true,
+				'SaveToDisk'      => opts['LoadFromDisk']))
+			client.add_extension(mod)
+		end
 
 		return true
 	end

user_interfaces/tc.rb
@@ -2,5 +2,7 @@
 
 require 'test/unit/ui/console/testrunner'
 require 'Msf/Core.rb.ts'
+require 'Msf/Base.rb.ts'
 
-Test::Unit::UI::Console::TestRunner.run(Msf::TestSuite)
+Test::Unit::UI::Console::TestRunner.run(Msf::TestSuite
+Test::Unit::UI::Console::TestRunner.run(Msf::Base::TestSuite)