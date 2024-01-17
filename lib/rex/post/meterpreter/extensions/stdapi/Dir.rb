ZedraGroup/zedrasploit-framework

implemented Dir
git-svn-id: file:///home/svn/incoming/trunk@2362 4d416f70-5f16-0410-b530-b9f4589650da

lib/rex/post/zeterpreter/extensions/stdapi/Dir.rb
@@ -1,0 +1,44 @@
+#!/usr/bin/ruby
+
+require 'Rex/Post/Dir'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Stdapi'
+
+module Rex
+module Post
+module Zeterpreter
+module Extensions
+module Stdapi
+
+TLV_TYPE_DIRECTORY_PATH = TLV_META_TYPE_STRING | 1200
+TLV_TYPE_FILE_NAME      = TLV_META_TYPE_STRING | 1201
+TLV_TYPE_FILE_PATH      = TLV_META_TYPE_STRING | 1202
+
+class Dir < Rex::Post::Dir
+
+	class <<self
+		attr_accessor :client
+	end
+
+=begin
+	entries(name)
+
+	Enumerates all of the files/folders in a given directory.
+=end
+	def Dir.entries(name)
+		request = Packet.create_request('stdapi_fs_ls')
+		files   = []
+
+		request.add_tlv(TLV_TYPE_DIRECTORY_PATH, name)
+
+		response = client.send_request(request)
+
+		response.each(TLV_TYPE_FILE_NAME) { |file_name|
+			files << file_name.value
+		}
+		
+		return files
+	end
+
+end
+
+end; end; end; end; end

lib/rex/post/zeterpreter/extensions/stdapi/stdapi.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Zeterpreter/Extension'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Dir'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Process'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Registry'
 
@@ -24,6 +25,7 @@ class Stdapi < Extension
 
 		# Alias the following things on the client object so that they
 		# can be directly referenced
+		client.register_extension_alias('dir', self.dir)
 		client.register_extension_alias('process', self.process)
 		client.register_extension_alias('registry', self.registry)
 	end
@@ -32,6 +34,10 @@ class Stdapi < Extension
 		klass = klass.dup
 		klass.client = self.client
 		return klass
+	end
+
+	def dir
+		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::Dir)
 	end
 
 	def process

radar-webhook-testing/metasploit-framework-commits

lib/rex/post/meterpreter/extensions/stdapi/Dir.rb
@@ -1,0 +1,44 @@
+#!/usr/bin/ruby
+
+require 'Rex/Post/Dir'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Stdapi'
+
+module Rex
+module Post
+module Meterpreter
+module Extensions
+module Stdapi
+
+TLV_TYPE_DIRECTORY_PATH = TLV_META_TYPE_STRING | 1200
+TLV_TYPE_FILE_NAME      = TLV_META_TYPE_STRING | 1201
+TLV_TYPE_FILE_PATH      = TLV_META_TYPE_STRING | 1202
+
+class Dir < Rex::Post::Dir
+
+	class <<self
+		attr_accessor :client
+	end
+
+=begin
+	entries(name)
+
+	Enumerates all of the files/folders in a given directory.
+=end
+	def Dir.entries(name)
+		request = Packet.create_request('stdapi_fs_ls')
+		files   = []
+
+		request.add_tlv(TLV_TYPE_DIRECTORY_PATH, name)
+
+		response = client.send_request(request)
+
+		response.each(TLV_TYPE_FILE_NAME) { |file_name|
+			files << file_name.value
+		}
+		
+		return files
+	end
+
+end
+
+end; end; end; end; end

lib/rex/post/meterpreter/extensions/stdapi/stdapi.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Meterpreter/Extension'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Dir'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Process'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Registry'
 
@@ -24,6 +25,7 @@ class Stdapi < Extension
 
 		# Alias the following things on the client object so that they
 		# can be directly referenced
+		client.register_extension_alias('dir', self.dir)
 		client.register_extension_alias('process', self.process)
 		client.register_extension_alias('registry', self.registry)
 	end
@@ -32,6 +34,10 @@ class Stdapi < Extension
 		klass = klass.dup
 		klass.client = self.client
 		return klass
+	end
+
+	def dir
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Dir)
 	end
 
 	def process