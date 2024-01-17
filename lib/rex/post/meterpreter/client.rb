ZedraGroup/zedrasploit-framework

file i/o working
git-svn-id: file:///home/svn/incoming/trunk@2365 4d416f70-5f16-0410-b530-b9f4589650da

lib/rex/post/zeterpreter/client.rb
@@ -2,6 +2,7 @@
 
 require 'socket'
 require 'Rex/Post/Zeterpreter/ClientCore'
+require 'Rex/Post/Zeterpreter/Channel'
 require 'Rex/Post/Zeterpreter/ObjectAliases'
 require 'Rex/Post/Zeterpreter/Packet'
 require 'Rex/Post/Zeterpreter/PacketParser'

lib/rex/post/zeterpreter/extensions/stdapi/File.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/File'
+require 'Rex/Post/Zeterpreter/Channel'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Stdapi'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/IO'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/FileStat'
 
 module Rex
 module Post
@@ -9,7 +12,7 @@ module Zeterpreter
 module Extensions
 module Stdapi
 
-class File 
+class File < Rex::Post::Zeterpreter::Extensions::Stdapi::IO
 
 	include Rex::Post::File
 	
@@ -17,12 +20,21 @@ class File
 		attr_accessor :client
 	end
 
-	def initialize(name, mode = "r", perms = 0)
-		self.client = self.class.client
-	end
-
 	def File.stat(name)
 		return client.filestat.new(name)
+	end
+
+	def initialize(name, mode = "r", perms = 0)
+		self.client = self.class.client
+		self.filed  = _open(name, mode, perms)
+	end
+
+	def _open(name, mode = "r", perms = 0)
+		return Channel.create(self.client, 'stdapi_fs_file', 
+				CHANNEL_FLAG_SYNCHRONOUS, [
+					{ 'type' => TLV_TYPE_FILE_PATH, 'value' => name       },
+					{ 'type' => TLV_TYPE_FILE_MODE, 'value' => mode + "b" },
+				])
 	end
 
 	protected

lib/rex/post/zeterpreter/extensions/stdapi/IO.rb
@@ -1,0 +1,35 @@
+#!/usr/bin/ruby
+
+require 'Rex/Post/IO'
+
+module Rex
+module Post
+module Zeterpreter
+module Extensions
+module Stdapi
+
+class IO < Rex::Post::IO
+
+	def read(length = nil, flags = nil)
+		recv(length, flags)
+	end
+
+	def recv(length = nil, flags = nil)
+		filed.recv(length)
+	end
+
+	def write(buf, length = nil, flags = nil)
+		send(buf, length, flags)
+	end
+
+	def send(buf, length = nil, flags = nil)
+		filed.send(buf, length)
+	end
+
+	def close
+		filed.close
+	end
+
+end
+
+end; end; end; end; end

radar-webhook-testing/metasploit-framework-commits

lib/rex/post/meterpreter/client.rb
@@ -2,6 +2,7 @@
 
 require 'socket'
 require 'Rex/Post/Meterpreter/ClientCore'
+require 'Rex/Post/Meterpreter/Channel'
 require 'Rex/Post/Meterpreter/ObjectAliases'
 require 'Rex/Post/Meterpreter/Packet'
 require 'Rex/Post/Meterpreter/PacketParser'

lib/rex/post/meterpreter/extensions/stdapi/File.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/File'
+require 'Rex/Post/Meterpreter/Channel'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Stdapi'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/IO'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/FileStat'
 
 module Rex
 module Post
@@ -9,7 +12,7 @@ module Meterpreter
 module Extensions
 module Stdapi
 
-class File 
+class File < Rex::Post::Meterpreter::Extensions::Stdapi::IO
 
 	include Rex::Post::File
 	
@@ -17,12 +20,21 @@ class File
 		attr_accessor :client
 	end
 
-	def initialize(name, mode = "r", perms = 0)
-		self.client = self.class.client
-	end
-
 	def File.stat(name)
 		return client.filestat.new(name)
+	end
+
+	def initialize(name, mode = "r", perms = 0)
+		self.client = self.class.client
+		self.filed  = _open(name, mode, perms)
+	end
+
+	def _open(name, mode = "r", perms = 0)
+		return Channel.create(self.client, 'stdapi_fs_file', 
+				CHANNEL_FLAG_SYNCHRONOUS, [
+					{ 'type' => TLV_TYPE_FILE_PATH, 'value' => name       },
+					{ 'type' => TLV_TYPE_FILE_MODE, 'value' => mode + "b" },
+				])
 	end
 
 	protected

lib/rex/post/meterpreter/extensions/stdapi/IO.rb
@@ -1,0 +1,35 @@
+#!/usr/bin/ruby
+
+require 'Rex/Post/IO'
+
+module Rex
+module Post
+module Meterpreter
+module Extensions
+module Stdapi
+
+class IO < Rex::Post::IO
+
+	def read(length = nil, flags = nil)
+		recv(length, flags)
+	end
+
+	def recv(length = nil, flags = nil)
+		filed.recv(length)
+	end
+
+	def write(buf, length = nil, flags = nil)
+		send(buf, length, flags)
+	end
+
+	def send(buf, length = nil, flags = nil)
+		filed.send(buf, length)
+	end
+
+	def close
+		filed.close
+	end
+
+end
+
+end; end; end; end; end