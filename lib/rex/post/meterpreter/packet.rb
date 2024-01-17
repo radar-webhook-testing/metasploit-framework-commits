ZedraGroup/zedrasploit-framework

moved constants
git-svn-id: file:///home/svn/incoming/trunk@2353 4d416f70-5f16-0410-b530-b9f4589650da

lib/rex/post/zeterpreter/packet.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/ruby
 
-def gen_tlv(meta, spec)
-	return meta | spec	
-end
+module Rex
+module Post
+module Zeterpreter
 
 #
 # Constants
@@ -26,33 +26,33 @@ TLV_META_TYPE_COMPLEX       = (1 << 31)
 #
 # TLV Specific Types
 #
-TLV_TYPE_ANY                = gen_tlv(TLV_META_TYPE_NONE,     0)
-TLV_TYPE_METHOD             = gen_tlv(TLV_META_TYPE_STRING,   1)
-TLV_TYPE_REQUEST_ID         = gen_tlv(TLV_META_TYPE_STRING,   2)
-TLV_TYPE_EXCEPTION          = gen_tlv(TLV_META_TYPE_GROUP,    3)
-TLV_TYPE_RESULT             = gen_tlv(TLV_META_TYPE_UINT,     4)
+TLV_TYPE_ANY                = TLV_META_TYPE_NONE   |   0
+TLV_TYPE_METHOD             = TLV_META_TYPE_STRING |   1
+TLV_TYPE_REQUEST_ID         = TLV_META_TYPE_STRING |   2
+TLV_TYPE_EXCEPTION          = TLV_META_TYPE_GROUP  |   3
+TLV_TYPE_RESULT             = TLV_META_TYPE_UINT   |   4
 
-TLV_TYPE_STRING             = gen_tlv(TLV_META_TYPE_STRING,  10)
-TLV_TYPE_UINT               = gen_tlv(TLV_META_TYPE_UINT,    11)
-TLV_TYPE_BOOL               = gen_tlv(TLV_META_TYPE_BOOL,    12)
+TLV_TYPE_STRING             = TLV_META_TYPE_STRING |  10
+TLV_TYPE_UINT               = TLV_META_TYPE_UINT   |  11
+TLV_TYPE_BOOL               = TLV_META_TYPE_BOOL   |  12
 
-TLV_TYPE_LENGTH             = gen_tlv(TLV_META_TYPE_UINT,    25)
-TLV_TYPE_DATA               = gen_tlv(TLV_META_TYPE_RAW,     26)
-TLV_TYPE_FLAGS              = gen_tlv(TLV_META_TYPE_UINT,    27)
+TLV_TYPE_LENGTH             = TLV_META_TYPE_UINT   |  25
+TLV_TYPE_DATA               = TLV_META_TYPE_RAW    |  26
+TLV_TYPE_FLAGS              = TLV_META_TYPE_UINT   |  27
 
-TLV_TYPE_CHANNEL_ID         = gen_tlv(TLV_META_TYPE_UINT,    50)
-TLV_TYPE_CHANNEL_TYPE       = gen_tlv(TLV_META_TYPE_STRING,  51)
-TLV_TYPE_CHANNEL_DATA       = gen_tlv(TLV_META_TYPE_RAW,     52)
-TLV_TYPE_CHANNEL_DATA_GROUP = gen_tlv(TLV_META_TYPE_GROUP,   53)
+TLV_TYPE_CHANNEL_ID         = TLV_META_TYPE_UINT   |  50
+TLV_TYPE_CHANNEL_TYPE       = TLV_META_TYPE_STRING |  51
+TLV_TYPE_CHANNEL_DATA       = TLV_META_TYPE_RAW    |  52
+TLV_TYPE_CHANNEL_DATA_GROUP = TLV_META_TYPE_GROUP  |  53
 
-TLV_TYPE_EXCEPTION_CODE     = gen_tlv(TLV_META_TYPE_UINT,   300)
-TLV_TYPE_EXCEPTION_STRING   = gen_tlv(TLV_META_TYPE_STRING, 301)
+TLV_TYPE_EXCEPTION_CODE     = TLV_META_TYPE_UINT   | 300
+TLV_TYPE_EXCEPTION_STRING   = TLV_META_TYPE_STRING | 301
 
-TLV_TYPE_LIBRARY_PATH       = gen_tlv(TLV_META_TYPE_STRING, 400)
-TLV_TYPE_TARGET_PATH        = gen_tlv(TLV_META_TYPE_STRING, 401)
+TLV_TYPE_LIBRARY_PATH       = TLV_META_TYPE_STRING | 400
+TLV_TYPE_TARGET_PATH        = TLV_META_TYPE_STRING | 401
 
-TLV_TYPE_CIPHER_NAME        = gen_tlv(TLV_META_TYPE_STRING, 500)
-TLV_TYPE_CIPHER_PARAMETERS  = gen_tlv(TLV_META_TYPE_GROUP,  501)
+TLV_TYPE_CIPHER_NAME        = TLV_META_TYPE_STRING | 500
+TLV_TYPE_CIPHER_PARAMETERS  = TLV_META_TYPE_GROUP  | 501
 
 #
 # Core flags
@@ -61,9 +61,6 @@ LOAD_LIBRARY_FLAG_ON_DISK   = (1 << 0)
 LOAD_LIBRARY_FLAG_EXTENSION = (1 << 1)
 LOAD_LIBRARY_FLAG_LOCAL     = (1 << 2)
 
-module Rex
-module Post
-module Zeterpreter
 
 ###
 #

radar-webhook-testing/metasploit-framework-commits

lib/rex/post/meterpreter/packet.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/ruby
 
-def gen_tlv(meta, spec)
-	return meta | spec	
-end
+module Rex
+module Post
+module Meterpreter
 
 #
 # Constants
@@ -26,33 +26,33 @@ TLV_META_TYPE_COMPLEX       = (1 << 31)
 #
 # TLV Specific Types
 #
-TLV_TYPE_ANY                = gen_tlv(TLV_META_TYPE_NONE,     0)
-TLV_TYPE_METHOD             = gen_tlv(TLV_META_TYPE_STRING,   1)
-TLV_TYPE_REQUEST_ID         = gen_tlv(TLV_META_TYPE_STRING,   2)
-TLV_TYPE_EXCEPTION          = gen_tlv(TLV_META_TYPE_GROUP,    3)
-TLV_TYPE_RESULT             = gen_tlv(TLV_META_TYPE_UINT,     4)
+TLV_TYPE_ANY                = TLV_META_TYPE_NONE   |   0
+TLV_TYPE_METHOD             = TLV_META_TYPE_STRING |   1
+TLV_TYPE_REQUEST_ID         = TLV_META_TYPE_STRING |   2
+TLV_TYPE_EXCEPTION          = TLV_META_TYPE_GROUP  |   3
+TLV_TYPE_RESULT             = TLV_META_TYPE_UINT   |   4
 
-TLV_TYPE_STRING             = gen_tlv(TLV_META_TYPE_STRING,  10)
-TLV_TYPE_UINT               = gen_tlv(TLV_META_TYPE_UINT,    11)
-TLV_TYPE_BOOL               = gen_tlv(TLV_META_TYPE_BOOL,    12)
+TLV_TYPE_STRING             = TLV_META_TYPE_STRING |  10
+TLV_TYPE_UINT               = TLV_META_TYPE_UINT   |  11
+TLV_TYPE_BOOL               = TLV_META_TYPE_BOOL   |  12
 
-TLV_TYPE_LENGTH             = gen_tlv(TLV_META_TYPE_UINT,    25)
-TLV_TYPE_DATA               = gen_tlv(TLV_META_TYPE_RAW,     26)
-TLV_TYPE_FLAGS              = gen_tlv(TLV_META_TYPE_UINT,    27)
+TLV_TYPE_LENGTH             = TLV_META_TYPE_UINT   |  25
+TLV_TYPE_DATA               = TLV_META_TYPE_RAW    |  26
+TLV_TYPE_FLAGS              = TLV_META_TYPE_UINT   |  27
 
-TLV_TYPE_CHANNEL_ID         = gen_tlv(TLV_META_TYPE_UINT,    50)
-TLV_TYPE_CHANNEL_TYPE       = gen_tlv(TLV_META_TYPE_STRING,  51)
-TLV_TYPE_CHANNEL_DATA       = gen_tlv(TLV_META_TYPE_RAW,     52)
-TLV_TYPE_CHANNEL_DATA_GROUP = gen_tlv(TLV_META_TYPE_GROUP,   53)
+TLV_TYPE_CHANNEL_ID         = TLV_META_TYPE_UINT   |  50
+TLV_TYPE_CHANNEL_TYPE       = TLV_META_TYPE_STRING |  51
+TLV_TYPE_CHANNEL_DATA       = TLV_META_TYPE_RAW    |  52
+TLV_TYPE_CHANNEL_DATA_GROUP = TLV_META_TYPE_GROUP  |  53
 
-TLV_TYPE_EXCEPTION_CODE     = gen_tlv(TLV_META_TYPE_UINT,   300)
-TLV_TYPE_EXCEPTION_STRING   = gen_tlv(TLV_META_TYPE_STRING, 301)
+TLV_TYPE_EXCEPTION_CODE     = TLV_META_TYPE_UINT   | 300
+TLV_TYPE_EXCEPTION_STRING   = TLV_META_TYPE_STRING | 301
 
-TLV_TYPE_LIBRARY_PATH       = gen_tlv(TLV_META_TYPE_STRING, 400)
-TLV_TYPE_TARGET_PATH        = gen_tlv(TLV_META_TYPE_STRING, 401)
+TLV_TYPE_LIBRARY_PATH       = TLV_META_TYPE_STRING | 400
+TLV_TYPE_TARGET_PATH        = TLV_META_TYPE_STRING | 401
 
-TLV_TYPE_CIPHER_NAME        = gen_tlv(TLV_META_TYPE_STRING, 500)
-TLV_TYPE_CIPHER_PARAMETERS  = gen_tlv(TLV_META_TYPE_GROUP,  501)
+TLV_TYPE_CIPHER_NAME        = TLV_META_TYPE_STRING | 500
+TLV_TYPE_CIPHER_PARAMETERS  = TLV_META_TYPE_GROUP  | 501
 
 #
 # Core flags
@@ -61,9 +61,6 @@ LOAD_LIBRARY_FLAG_ON_DISK   = (1 << 0)
 LOAD_LIBRARY_FLAG_EXTENSION = (1 << 1)
 LOAD_LIBRARY_FLAG_LOCAL     = (1 << 2)
 
-module Rex
-module Post
-module Meterpreter
 
 ###
 #