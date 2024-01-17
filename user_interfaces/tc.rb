ZedraGroup/zedrasploit-framework

added initial data store implementation and test case runner
git-svn-id: file:///home/svn/incoming/trunk@2496 4d416f70-5f16-0410-b530-b9f4589650da

user_interfaces/tc.rb
@@ -1,0 +1,6 @@
+#!/usr/bin/ruby -I../Framework
+
+require 'test/unit/ui/console/testrunner'
+require 'Zsf'
+
+Test::Unit::UI::Console::TestRunner.run(Zsf::Test::FrameworkCoreTestSuite)

radar-webhook-testing/metasploit-framework-commits

user_interfaces/tc.rb
@@ -1,0 +1,6 @@
+#!/usr/bin/ruby -I../Framework
+
+require 'test/unit/ui/console/testrunner'
+require 'Msf'
+
+Test::Unit::UI::Console::TestRunner.run(Msf::Test::FrameworkCoreTestSuite)