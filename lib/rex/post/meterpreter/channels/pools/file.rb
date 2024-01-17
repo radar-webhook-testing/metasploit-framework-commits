ZedraGroup/zedrasploit-framework

more channel foo
git-svn-id: file:///home/svn/incoming/trunk@2376 4d416f70-5f16-0410-b530-b9f4589650da

lib/rex/post/zeterpreter/channels/pools/file.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Zeterpreter/Channels/Pool'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Tlv'
 
 module Rex
 module Post
@@ -18,6 +19,27 @@ module Pools
 #
 ###
 class File < Rex::Post::Zeterpreter::Channels::Pool
+
+	##
+	#
+	# Factory
+	#
+	##
+
+	def File.open(client, name, mode = "r", perm = 0)
+		return Channel.create(client, 'stdapi_fs_file', 
+				self, CHANNEL_FLAG_SYNCHRONOUS, 
+				[
+					{ 
+						'type'  => Rex::Post::Zeterpreter::Extensions::Stdapi::TLV_TYPE_FILE_PATH, 
+						'value' => name       
+					},
+					{ 
+						'type'  => Rex::Post::Zeterpreter::Extensions::Stdapi::TLV_TYPE_FILE_MODE, 
+						'value' => mode + "b" 
+					},
+				])
+	end
 
 	##
 	#

lib/rex/post/zeterpreter/channels/stream.rb
@@ -1,0 +1,59 @@
+#!/usr/bin/ruby
+
+require 'Rex/Socket/StreamAbstraction'
+
+module Rex
+module Post
+module Zeterpreter
+
+###
+#
+# Stream
+# ------
+#
+# This class represents a channel that is streaming.  This means
+# that sequential data is flowing in either one or both directions.
+#
+###
+class Stream
+
+	include Rex::Socket::StreamAbstraction
+
+	class <<self
+		def cls
+			return CHANNEL_CLASS_STREAM
+		end
+	end
+
+	##
+	#
+	# Constructor
+	#
+	##
+	
+	# Passes the initialization information up to the base class
+	def initialize(client, cid, type, flags)
+		super(client, cid, type, flags)
+	end
+
+	##
+	#
+	# I/O handlers
+	#
+	##
+
+	def dio_write_handler(packet, data)
+		rsock.write(data)
+
+		return true;
+	end
+
+	def dio_close_handler(packet)
+		rsock.close
+		
+		return super(packet)
+	end
+
+end
+
+end; end; end

lib/rex/post/zeterpreter/channels/streams/Tcp.rb
@@ -1,0 +1,48 @@
+#!/usr/bin/ruby
+
+require 'Rex/Post/Zeterpreter/Channels/Stream'
+
+module Rex
+module Post
+module Zeterpreter
+module Channels
+
+###
+#
+# Tcp
+# ---
+#
+# The TCP class wrappers a stream-based zeterpreter channel.
+#
+###
+class Tcp < Rex::Post::Zeterpreter::Channels::Stream
+
+	##
+	#
+	# Factory
+	#
+	##
+
+	def Tcp.open(client, host, port)
+		return Channel.create(client, 'net_stream_tcp',
+			self, CHANNEL_FLAG_SYNCHRONOUS,
+			[
+				{ 'type' => 
+
+			]
+	end
+
+	##
+	#
+	# Constructor
+	#
+	##
+
+	# Passes the initialization information up to the base class
+	def initialize(client, cid, type, flags)
+		super(client, cid, type, flags)
+	end
+
+end
+
+end; end; end; end

lib/rex/post/zeterpreter/extensions/stdapi/File.rb
@@ -47,7 +47,7 @@ class File < Rex::Post::Zeterpreter::Extensions::Stdapi::IO
 	def eof
 		return self.filed.eof
 	end
-	
+
 	# Returns the current position of the file pointer
 	def pos
 		return self.filed.tell
@@ -73,13 +73,8 @@ protected
 
 	# Creates a File channel using the supplied information
 	def _open(name, mode = "r", perms = 0)
-		return Channel.create(self.client, 'stdapi_fs_file', 
-				Rex::Post::Zeterpreter::Channels::Pools::File,
-				CHANNEL_FLAG_SYNCHRONOUS, 
-				[
-					{ 'type' => TLV_TYPE_FILE_PATH, 'value' => name       },
-					{ 'type' => TLV_TYPE_FILE_MODE, 'value' => mode + "b" },
-				])
+		return Rex::Post::Zeterpreter::Channels::Pools::File.open(
+				self.client, name, mode, perms)
 	end
 
 	attr_accessor :client

lib/rex/post/zeterpreter/extensions/stdapi/stdapi.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Zeterpreter/Extension'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Tlv'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Dir'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/File'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/FileStat'
@@ -12,28 +13,6 @@ module Post
 module Zeterpreter
 module Extensions
 module Stdapi
-
-# Process
-TLV_TYPE_PID            = TLV_META_TYPE_UINT    |    0
-
-# Registry
-TLV_TYPE_HKEY           = TLV_META_TYPE_UINT    | 1000
-TLV_TYPE_ROOT_KEY       = TLV_TYPE_HKEY
-TLV_TYPE_BASE_KEY       = TLV_META_TYPE_STRING  | 1001
-TLV_TYPE_PERMISSION     = TLV_META_TYPE_UINT    | 1002
-TLV_TYPE_KEY_NAME       = TLV_META_TYPE_STRING  | 1003
-TLV_TYPE_VALUE_NAME     = TLV_META_TYPE_STRING  | 1010
-TLV_TYPE_VALUE_TYPE     = TLV_META_TYPE_UINT    | 1011
-TLV_TYPE_VALUE_DATA     = TLV_META_TYPE_RAW     | 1012
-
-# Fs
-TLV_TYPE_DIRECTORY_PATH = TLV_META_TYPE_STRING  | 1200
-TLV_TYPE_FILE_NAME      = TLV_META_TYPE_STRING  | 1201
-TLV_TYPE_FILE_PATH      = TLV_META_TYPE_STRING  | 1202
-TLV_TYPE_FILE_MODE      = TLV_META_TYPE_STRING  | 1203
-TLV_TYPE_STAT_BUF       = TLV_META_TYPE_COMPLEX | 1220
-
-DELETE_KEY_FLAG_RECURSIVE = (1 << 0)
 
 ###
 #

lib/rex/post/zeterpreter/extensions/stdapi/tlv.rb
@@ -1,0 +1,31 @@
+#!/usr/bin/ruby
+
+module Rex
+module Post
+module Zeterpreter
+module Extensions
+module Stdapi
+
+# Process
+TLV_TYPE_PID            = TLV_META_TYPE_UINT    |    0
+
+# Registry
+TLV_TYPE_HKEY           = TLV_META_TYPE_UINT    | 1000
+TLV_TYPE_ROOT_KEY       = TLV_TYPE_HKEY
+TLV_TYPE_BASE_KEY       = TLV_META_TYPE_STRING  | 1001
+TLV_TYPE_PERMISSION     = TLV_META_TYPE_UINT    | 1002
+TLV_TYPE_KEY_NAME       = TLV_META_TYPE_STRING  | 1003
+TLV_TYPE_VALUE_NAME     = TLV_META_TYPE_STRING  | 1010
+TLV_TYPE_VALUE_TYPE     = TLV_META_TYPE_UINT    | 1011
+TLV_TYPE_VALUE_DATA     = TLV_META_TYPE_RAW     | 1012
+
+# Fs
+TLV_TYPE_DIRECTORY_PATH = TLV_META_TYPE_STRING  | 1200
+TLV_TYPE_FILE_NAME      = TLV_META_TYPE_STRING  | 1201
+TLV_TYPE_FILE_PATH      = TLV_META_TYPE_STRING  | 1202
+TLV_TYPE_FILE_MODE      = TLV_META_TYPE_STRING  | 1203
+TLV_TYPE_STAT_BUF       = TLV_META_TYPE_COMPLEX | 1220
+
+DELETE_KEY_FLAG_RECURSIVE = (1 << 0)
+
+end; end; end; end; end

radar-webhook-testing/metasploit-framework-commits

lib/rex/post/meterpreter/channels/pools/file.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Meterpreter/Channels/Pool'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Tlv'
 
 module Rex
 module Post
@@ -18,6 +19,27 @@ module Pools
 #
 ###
 class File < Rex::Post::Meterpreter::Channels::Pool
+
+	##
+	#
+	# Factory
+	#
+	##
+
+	def File.open(client, name, mode = "r", perm = 0)
+		return Channel.create(client, 'stdapi_fs_file', 
+				self, CHANNEL_FLAG_SYNCHRONOUS, 
+				[
+					{ 
+						'type'  => Rex::Post::Meterpreter::Extensions::Stdapi::TLV_TYPE_FILE_PATH, 
+						'value' => name       
+					},
+					{ 
+						'type'  => Rex::Post::Meterpreter::Extensions::Stdapi::TLV_TYPE_FILE_MODE, 
+						'value' => mode + "b" 
+					},
+				])
+	end
 
 	##
 	#

lib/rex/post/meterpreter/channels/stream.rb
@@ -1,0 +1,59 @@
+#!/usr/bin/ruby
+
+require 'Rex/Socket/StreamAbstraction'
+
+module Rex
+module Post
+module Meterpreter
+
+###
+#
+# Stream
+# ------
+#
+# This class represents a channel that is streaming.  This means
+# that sequential data is flowing in either one or both directions.
+#
+###
+class Stream
+
+	include Rex::Socket::StreamAbstraction
+
+	class <<self
+		def cls
+			return CHANNEL_CLASS_STREAM
+		end
+	end
+
+	##
+	#
+	# Constructor
+	#
+	##
+	
+	# Passes the initialization information up to the base class
+	def initialize(client, cid, type, flags)
+		super(client, cid, type, flags)
+	end
+
+	##
+	#
+	# I/O handlers
+	#
+	##
+
+	def dio_write_handler(packet, data)
+		rsock.write(data)
+
+		return true;
+	end
+
+	def dio_close_handler(packet)
+		rsock.close
+		
+		return super(packet)
+	end
+
+end
+
+end; end; end

lib/rex/post/meterpreter/channels/streams/Tcp.rb
@@ -1,0 +1,48 @@
+#!/usr/bin/ruby
+
+require 'Rex/Post/Meterpreter/Channels/Stream'
+
+module Rex
+module Post
+module Meterpreter
+module Channels
+
+###
+#
+# Tcp
+# ---
+#
+# The TCP class wrappers a stream-based meterpreter channel.
+#
+###
+class Tcp < Rex::Post::Meterpreter::Channels::Stream
+
+	##
+	#
+	# Factory
+	#
+	##
+
+	def Tcp.open(client, host, port)
+		return Channel.create(client, 'net_stream_tcp',
+			self, CHANNEL_FLAG_SYNCHRONOUS,
+			[
+				{ 'type' => 
+
+			]
+	end
+
+	##
+	#
+	# Constructor
+	#
+	##
+
+	# Passes the initialization information up to the base class
+	def initialize(client, cid, type, flags)
+		super(client, cid, type, flags)
+	end
+
+end
+
+end; end; end; end

lib/rex/post/meterpreter/extensions/stdapi/File.rb
@@ -73,13 +73,8 @@ protected
 
 	# Creates a File channel using the supplied information
 	def _open(name, mode = "r", perms = 0)
-		return Channel.create(self.client, 'stdapi_fs_file', 
-				Rex::Post::Meterpreter::Channels::Pools::File,
-				CHANNEL_FLAG_SYNCHRONOUS, 
-				[
-					{ 'type' => TLV_TYPE_FILE_PATH, 'value' => name       },
-					{ 'type' => TLV_TYPE_FILE_MODE, 'value' => mode + "b" },
-				])
+		return Rex::Post::Meterpreter::Channels::Pools::File.open(
+				self.client, name, mode, perms)
 	end
 
 	attr_accessor :client

lib/rex/post/meterpreter/extensions/stdapi/stdapi.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Rex/Post/Meterpreter/Extension'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Tlv'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Dir'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/File'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/FileStat'
@@ -12,28 +13,6 @@ module Post
 module Meterpreter
 module Extensions
 module Stdapi
-
-# Process
-TLV_TYPE_PID            = TLV_META_TYPE_UINT    |    0
-
-# Registry
-TLV_TYPE_HKEY           = TLV_META_TYPE_UINT    | 1000
-TLV_TYPE_ROOT_KEY       = TLV_TYPE_HKEY
-TLV_TYPE_BASE_KEY       = TLV_META_TYPE_STRING  | 1001
-TLV_TYPE_PERMISSION     = TLV_META_TYPE_UINT    | 1002
-TLV_TYPE_KEY_NAME       = TLV_META_TYPE_STRING  | 1003
-TLV_TYPE_VALUE_NAME     = TLV_META_TYPE_STRING  | 1010
-TLV_TYPE_VALUE_TYPE     = TLV_META_TYPE_UINT    | 1011
-TLV_TYPE_VALUE_DATA     = TLV_META_TYPE_RAW     | 1012
-
-# Fs
-TLV_TYPE_DIRECTORY_PATH = TLV_META_TYPE_STRING  | 1200
-TLV_TYPE_FILE_NAME      = TLV_META_TYPE_STRING  | 1201
-TLV_TYPE_FILE_PATH      = TLV_META_TYPE_STRING  | 1202
-TLV_TYPE_FILE_MODE      = TLV_META_TYPE_STRING  | 1203
-TLV_TYPE_STAT_BUF       = TLV_META_TYPE_COMPLEX | 1220
-
-DELETE_KEY_FLAG_RECURSIVE = (1 << 0)
 
 ###
 #

lib/rex/post/meterpreter/extensions/stdapi/tlv.rb
@@ -1,0 +1,31 @@
+#!/usr/bin/ruby
+
+module Rex
+module Post
+module Meterpreter
+module Extensions
+module Stdapi
+
+# Process
+TLV_TYPE_PID            = TLV_META_TYPE_UINT    |    0
+
+# Registry
+TLV_TYPE_HKEY           = TLV_META_TYPE_UINT    | 1000
+TLV_TYPE_ROOT_KEY       = TLV_TYPE_HKEY
+TLV_TYPE_BASE_KEY       = TLV_META_TYPE_STRING  | 1001
+TLV_TYPE_PERMISSION     = TLV_META_TYPE_UINT    | 1002
+TLV_TYPE_KEY_NAME       = TLV_META_TYPE_STRING  | 1003
+TLV_TYPE_VALUE_NAME     = TLV_META_TYPE_STRING  | 1010
+TLV_TYPE_VALUE_TYPE     = TLV_META_TYPE_UINT    | 1011
+TLV_TYPE_VALUE_DATA     = TLV_META_TYPE_RAW     | 1012
+
+# Fs
+TLV_TYPE_DIRECTORY_PATH = TLV_META_TYPE_STRING  | 1200
+TLV_TYPE_FILE_NAME      = TLV_META_TYPE_STRING  | 1201
+TLV_TYPE_FILE_PATH      = TLV_META_TYPE_STRING  | 1202
+TLV_TYPE_FILE_MODE      = TLV_META_TYPE_STRING  | 1203
+TLV_TYPE_STAT_BUF       = TLV_META_TYPE_COMPLEX | 1220
+
+DELETE_KEY_FLAG_RECURSIVE = (1 << 0)
+
+end; end; end; end; end