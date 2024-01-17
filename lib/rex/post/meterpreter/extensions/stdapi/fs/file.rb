ZedraGroup/zedrasploit-framework

moved dir/file/filestat under fs
git-svn-id: file:///home/svn/incoming/trunk@2393 4d416f70-5f16-0410-b530-b9f4589650da

lib/rex/post/zeterpreter/extensions/stdapi/fs/file.rb
@@ -4,16 +4,17 @@ require 'Rex/Post/File'
 require 'Rex/Post/Zeterpreter/Channel'
 require 'Rex/Post/Zeterpreter/Channels/Pools/File'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Stdapi'
-require 'Rex/Post/Zeterpreter/Extensions/Stdapi/IO'
-require 'Rex/Post/Zeterpreter/Extensions/Stdapi/FileStat'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Fs/IO'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Fs/FileStat'
 
 module Rex
 module Post
 module Zeterpreter
 module Extensions
 module Stdapi
+module Fs
 
-class File < Rex::Post::Zeterpreter::Extensions::Stdapi::IO
+class File < Rex::Post::Zeterpreter::Extensions::Stdapi::Fs::IO
 
 	include Rex::Post::File
 	
@@ -22,7 +23,7 @@ class File < Rex::Post::Zeterpreter::Extensions::Stdapi::IO
 	end
 
 	def File.stat(name)
-		return client.filestat.new(name)
+		return client.fs.filestat.new(name)
 	end
 
 	##
@@ -81,4 +82,4 @@ protected
 
 end
 
-end; end; end; end; end
+end; end; end; end; end; end

lib/rex/post/zeterpreter/extensions/stdapi/stdapi.rb
@@ -4,9 +4,9 @@ require 'Rex/Post/Zeterpreter/ObjectAliases'
 require 'Rex/Post/Zeterpreter/Extension'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Constants'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Tlv'
-require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Dir'
-require 'Rex/Post/Zeterpreter/Extensions/Stdapi/File'
-require 'Rex/Post/Zeterpreter/Extensions/Stdapi/FileStat'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Fs/Dir'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Fs/File'
+require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Fs/FileStat'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/UI'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Sys/Process'
 require 'Rex/Post/Zeterpreter/Extensions/Stdapi/Sys/Registry'
@@ -54,7 +54,7 @@ class Stdapi < Extension
 					'name' => 'ui',
 					'ext'  => UI.new(client)
 				}
-				
+
 			])
 	end
 
@@ -67,17 +67,17 @@ class Stdapi < Extension
 
 	# Returns a copy of the Dir class
 	def dir
-		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::Dir)
+		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::Fs::Dir)
 	end
 
 	# Returns a copy of the File class
 	def file
-		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::File)
+		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::Fs::File)
 	end
 
 	# Returns a copy of the FileStat class
 	def filestat
-		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::FileStat)
+		brand(Rex::Post::Zeterpreter::Extensions::Stdapi::Fs::FileStat)
 	end
 
 	# Returns a copy of the Process class

radar-webhook-testing/metasploit-framework-commits

lib/rex/post/meterpreter/extensions/stdapi/fs/file.rb
@@ -4,16 +4,17 @@ require 'Rex/Post/File'
 require 'Rex/Post/Meterpreter/Channel'
 require 'Rex/Post/Meterpreter/Channels/Pools/File'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Stdapi'
-require 'Rex/Post/Meterpreter/Extensions/Stdapi/IO'
-require 'Rex/Post/Meterpreter/Extensions/Stdapi/FileStat'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Fs/IO'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Fs/FileStat'
 
 module Rex
 module Post
 module Meterpreter
 module Extensions
 module Stdapi
+module Fs
 
-class File < Rex::Post::Meterpreter::Extensions::Stdapi::IO
+class File < Rex::Post::Meterpreter::Extensions::Stdapi::Fs::IO
 
 	include Rex::Post::File
 	
@@ -22,7 +23,7 @@ class File < Rex::Post::Meterpreter::Extensions::Stdapi::IO
 	end
 
 	def File.stat(name)
-		return client.filestat.new(name)
+		return client.fs.filestat.new(name)
 	end
 
 	##
@@ -81,4 +82,4 @@ protected
 
 end
 
-end; end; end; end; end
+end; end; end; end; end; end

lib/rex/post/meterpreter/extensions/stdapi/stdapi.rb
@@ -4,9 +4,9 @@ require 'Rex/Post/Meterpreter/ObjectAliases'
 require 'Rex/Post/Meterpreter/Extension'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Constants'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Tlv'
-require 'Rex/Post/Meterpreter/Extensions/Stdapi/Dir'
-require 'Rex/Post/Meterpreter/Extensions/Stdapi/File'
-require 'Rex/Post/Meterpreter/Extensions/Stdapi/FileStat'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Fs/Dir'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Fs/File'
+require 'Rex/Post/Meterpreter/Extensions/Stdapi/Fs/FileStat'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/UI'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Sys/Process'
 require 'Rex/Post/Meterpreter/Extensions/Stdapi/Sys/Registry'
@@ -67,17 +67,17 @@ class Stdapi < Extension
 
 	# Returns a copy of the Dir class
 	def dir
-		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Dir)
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Fs::Dir)
 	end
 
 	# Returns a copy of the File class
 	def file
-		brand(Rex::Post::Meterpreter::Extensions::Stdapi::File)
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Fs::File)
 	end
 
 	# Returns a copy of the FileStat class
 	def filestat
-		brand(Rex::Post::Meterpreter::Extensions::Stdapi::FileStat)
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Fs::FileStat)
 	end
 
 	# Returns a copy of the Process class